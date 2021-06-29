6ce858ea5eec0e39aface4b28d1f84aa659e3e22
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-06-29 08:10:00 -0400

    up

--- log/equipos/equipo.php
+++ log/equipos/equipo.php
@@ -194,7 +194,7 @@
     //fecha actual del equipo
     //$fecha=date_default_timezone_get();
 
-    $sql3= "SELECT * from equipos WHERE eq_fecha>='$fecha'";
+    $sql3= "SELECT * from equipos WHERE eq_fecha>='$fecha' ORDER BY eq_fecha desc";
     $consulta3 = mysqli_query($conn,$sql3);
 
     while ($res=mysqli_fetch_array($consulta3)) {
