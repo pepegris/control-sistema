1b3393f245381bb0f7c001fc22ab4c46c73dc962
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-07-02 00:27:49 -0400

    up

--- log/config/menu_datos.php
+++ log/config/menu_datos.php
@@ -1,3 +1,24 @@
+<?php
+
+ 
+
+
+
+ session_start();
+
+$cuenta_on=$_SESSION['username'];
+
+if (!isset($cuenta_on)) {
+    header("location:../salir.php");
+}elseif ($cuenta_on != 'sistema') {
+        header("location:../salir.php");
+}
+
+$cuenta_on = ucwords($cuenta_on); 
+
+ 
+ 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
