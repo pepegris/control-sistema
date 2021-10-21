cac26cfb79b51235d021481ce0171bbed0822c87
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-10-21 14:39:55 -0400

    fiscal

--- log/config/menu_datos.php
+++ log/config/menu_datos.php
@@ -31,7 +31,7 @@ include '../includes/menu.php';
 
         <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="menu_sedes.php">Sede</a>
         <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="servidores.php">Servidores</a>
-        <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="#">Fiscales</a>
+        <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="fiscales.php">Fiscales</a>
         <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="menu_impresora.php">Impresora</a>
         <a class="item" style="font-family:Arial, Helvetica, sans-serif; font-size: 16px;" href="registrar.php" >Registrar Usuario</a>
        
