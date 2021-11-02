ed48b09d33ca9f66974d2d1aa3b76d710b49d623
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-11-02 11:21:14 -0430

    a

---
+++ log/config/servidores_buscar.php
@@ -0,0 +1,194 @@
+<?php require_once 'includes/log.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/formulario/formulario.css">
+    <link rel="stylesheet" href="../css/formulario/bootstrap.min.css">
+    <title>Conf Servidores</title>
+</head>
+<style>
+    #contadores {
+        margin-left: 95px;
+    }
+    table td{
+        color: black;
+    }
+    
+</style>
+<body>
+<?php 
+if (isset($_POST)) {
+
+    require '../includes/conexion_control.php';
+    include '../includes/cabecera.php';
+
+    $sedes_nom=isset ($_POST ['sedes_nom']) ? mysqli_real_escape_string($conn,$_POST ['sedes_nom'] ): false ;
+    $fecha_desde=isset($_POST ['fecha_desde']) ? mysqli_real_escape_string($conn,$_POST['fecha_desde']):false ;
+    $fecha_hasta=isset($_POST ['fecha_hasta']) ? mysqli_real_escape_string($conn,$_POST['fecha_hasta']):false ;
+    
+    
+ ?>
+
+ <center><h2>Servidores <?=$sedes_nom?></h2></center>
+<table  class='table table-hover' id="tblData" >
+        <thead>
+        
+            <tr class='table-primary'>
+  
+                <th scope='col' abbr='Starter'>Tienda</th>
+                <th scope='col' abbr='Starter'>Mac</th>
+                <th scope='col' abbr='Starter'>Memoria Ram</th>
+                <th scope='col' abbr='Starter'>Procesador</th>
+                <th scope='col' abbr='Starter'>Disco Duro</th>
+                <th scope='col' abbr='Starter'>Tarjeta Video</th>
+                <th scope='col' abbr='Starter'>Tarjeta Red</th>
+                <th scope='col' abbr='Starter'>Fecha</th>
+                <th scope='col' abbr='Starter'>Imagen</th>
+                <th scope='col' abbr='Starter'>Accion</th>
+                
+               
+				
+			<tr>
+		</thead>
+
+<?php 
+
+
+
+
+
+
+    if ($conn) {
+
+        $sql;
+        if ($sedes_nom) {
+
+
+                if ($fecha_desde && $fecha_hasta) {
+                    $sql="SELECT * from servidor_auditoria WHERE tienda='$sedes_nom' and serv_fecha BETWEEN '$fecha_desde' and '$fecha_hasta' order by serv_fecha desc ";
+
+                } elseif ($fecha_desde==null && !$fecha_hasta) {
+
+                    $sql="SELECT * from servidor_auditoria WHERE tienda='$sedes_nom'  order by serv_fecha desc ";
+
+                } elseif ($fecha_desde && !$fecha_hasta) {
+
+                    $sql="SELECT * from servidor_auditoria WHERE tienda='$sedes_nom' and serv_fecha >='$fecha_desde'  order by serv_fecha desc ";
+
+                }elseif (!$fecha_desde && $fecha_hasta) {
+
+                    $sql="SELECT * from servidor_auditoria WHERE tienda='$sedes_nom' and serv_fecha  <= '$fecha_hasta' order by serv_fecha desc ";
+
+                    
+                }
+           
+        }elseif ($sedes_nom==null) {
+
+
+                $sql="SELECT * from servidor_auditoria WHERE serv_fecha BETWEEN '$fecha_desde' and '$fecha_hasta' order by serv_fecha desc ";
+
+                if ($fecha_desde && $fecha_hasta==null) {
+
+                    $sql="SELECT * from servidor_auditoria WHERE serv_fecha >='$fecha_desde'  order by serv_fecha desc ";
+                }elseif ($fecha_desde==null && $fecha_hasta) {
+
+
+                    $sql="SELECT * from servidor_auditoria WHERE serv_fecha <='$fecha_hasta'  order by serv_fecha desc ";
+                }
+
+
+        }
+        
+
+       
+        $consulta= mysqli_query($conn,$sql);
+
+        while ($res=mysqli_fetch_array($consulta)) {
+
+            $id=$res['id'];
+            $tienda=$res['tienda'];
+       //     $descripcion=$res['serv_des'];
+            $mac=$res['serv_mac'];
+            $procesador=$res['serv_proc'];
+            $ram=$res['serv_ram'];
+            $disco=$res['serv_disc'];
+            $video=$res['serv_vid'];
+            $red=$res['serv_red'];
+            $imagen=$res['serv_img'];
+            $serv_fecha=$res['serv_fecha']; ?>
+    
+    <tr>
+
+   
+        <td><?=$tienda?></td>
+        <td><?=$mac?></td>
+        <td><?=$procesador?></td>
+        <td><?=$ram?></td>
+        <td><?=$disco?></td>
+        <td><?=$video?></td>
+        <td><?=$red?></td>
+        <td><?=$serv_fecha?></td>
+        <td><img src="../uploads/img/servidores/<?=$imagen?>" style="height: 90px;" alt=""></td>
+
+        <td><?php
+
+
+        if ($cuenta_on=='sistema') {
+            
+            echo "<a href='edit_servidor.php?id=<?php echo $id?>' class='btn btn-info'>
+            <i class='fas fa-marker'></i></a>";
+        }else {
+            echo "<a href='#' class='btn btn-info'>
+            <i class='fas fa-marker'></i></a>";
+        }
+
+        
+        
+        
+        ?>
+            <!-- <a href='delete_servidor.php?id=<?php echo $id?>' class='btn btn-danger'>
+                    <i class='far fa-trash-alt'></i>
+            </a> -->
+        </td>
+        
+     
+    </tr>
+      
+    
+
+
+
+        
+ <?php     }}}
+ 
+ 
+ include '../includes/excel.php';
+ 
+ 
+ 
+ 
+ ?>
+
+ </div>
+
+
+
+
+
+    
+</body>
+</html>
+
+
+
+<?php
+
+
+
+
+
+
+?>
---
+++ log/config/servidores_post.php
@@ -0,0 +1,138 @@
+
+<?php
+require_once 'includes/log.php';
+if ( isset($_POST)) {
+
+    include '../includes/loading.php';
+    require '../includes/conexion_control.php';
+
+        $nombre_imagen = $_FILES['imagen']['name'];
+        $tipo_imagen   = $_FILES['imagen']['type'];
+        $tam_imagen    = $_FILES['imagen']['size'];
+
+        $tienda=isset ($_POST ['tienda']) ? mysqli_real_escape_string($conn,$_POST ['tienda'] ): false ;
+        $serv_mac=isset($_POST ['serv_mac']) ? mysqli_real_escape_string($conn,$_POST['serv_mac']):false ;
+        $serv_proc=isset($_POST ['serv_proc']) ? mysqli_real_escape_string($conn,$_POST['serv_proc']):false ;
+        $serv_ram=isset($_POST ['serv_ram']) ? mysqli_real_escape_string($conn,$_POST['serv_ram']):false ;
+        $serv_disc=isset($_POST ['serv_disc']) ? mysqli_real_escape_string($conn,$_POST['serv_disc']):false ;
+        $serv_vid=isset($_POST ['serv_vid']) ? mysqli_real_escape_string($conn,$_POST['serv_vid']):false ;
+        $serv_red=isset($_POST ['serv_red']) ? mysqli_real_escape_string($conn,$_POST['serv_red']):false ;
+        $serv_des=isset($_POST ['serv_des']) ? mysqli_real_escape_string($conn,$_POST['serv_des']):false ;
+        $serv_fecha=isset($_POST ['serv_fecha']) ? mysqli_real_escape_string($conn,$_POST['serv_fecha']):false ;
+      
+        
+
+   if ($nombre_imagen == ''  ) {
+        if ($conn) {
+
+  // $sql= "INSERT INTO servidor_auditoria VALUES (null,'$tienda','$serv_des',null,'$serv_fecha',now())";
+    $sql= "UPDATE servidor_auditoria SET serv_mac='$serv_mac', serv_proc='$serv_proc', serv_ram='$serv_ram', serv_disc='$serv_disc', serv_vid='$serv_vid', serv_red='$serv_red', serv_des='$serv_des', serv_fecha='$serv_fecha' WHERE tienda ='$tienda' ";
+                    
+    $guardar = mysqli_query($conn,$sql);
+    echo "<br><center><h3>Cargando Servidor sin imagen</h3></center>";
+        if (!$guardar) {
+        
+        
+        //mandando mensaje de error de la base de datos
+                                        
+        $error= mysqli_error($conn);
+        echo "<br><center><h3>ERROR</h3></center>";
+        echo "<h4>$error</h4>";
+                                
+                                            
+        echo "<a href='servidores.php' class='btn btn-danger'>Salir</a>";
+        die();
+                                        
+                                            
+            }else {
+                header('refresh:2;url= servidores.php');
+                exit;
+            }
+                    
+                                
+                    
+    } else {
+                            
+                                            
+         echo "<a href='servidores.php' class='btn btn-danger'>Salir</a>";
+         die("La conexión ha fallado: " . mysqli_connect_error());
+    }
+        
+        
+        
+
+   }elseif ( $tipo_imagen=="image/jpeg" or $tipo_imagen=="image/jpg" or $tipo_imagen=="image/png" or $tipo_imagen=="image/gif" ) {
+   
+
+
+        //ruta del destino del servidor
+        $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/php/control/log/uploads/img/servidores/';
+
+
+
+        //almacenando nombre y direccion de la imagen
+
+        $imagen=$serv_fecha.'_'.$tienda.'_'.$nombre_imagen;
+
+        
+    
+        //mover imagen a directorio temporal
+    
+        move_uploaded_file($_FILES['imagen']['tmp_name'],$carpeta.$imagen);
+
+        if ($conn) {
+
+            //$sql= "INSERT INTO fiscal VALUES (null,'$tienda','$serv_des','$imagen','$serv_fecha',now())";
+            $sql= "UPDATE servidor_auditoria SET serv_mac='$serv_mac', serv_proc='$serv_proc', serv_ram='$serv_ram', serv_disc='$serv_disc', serv_vid='$serv_vid', serv_red='$serv_red', serv_des='$serv_des',serv_img='$imagen', serv_fecha='$serv_fecha' WHERE tienda ='$tienda' ";
+                            
+            $guardar = mysqli_query($conn,$sql);
+                            
+                if (!$guardar) {
+                
+                
+                //mandando mensaje de error de la base de datos
+                                                
+                $error= mysqli_error($conn);
+                echo "<br><center><h3>ERROR</h3></center>";
+                echo "<h4>$error</h4>";
+                                        
+                                                    
+                echo "<a href='servidores.php' class='btn btn-danger'>Salir</a>";
+                die();
+                                                
+                                                    
+                    }else {
+                        header('refresh:2;url= servidores.php');
+                        exit;
+                    }
+                            
+                                        
+                            
+            } else {
+                                    
+                                                    
+                 echo "<a href='servidores.php' class='btn btn-danger'>Salir</a>";
+                 die("La conexión ha fallado: " . mysqli_connect_error());
+            }
+
+
+
+
+    }else {
+
+        //REDIRECCIONAR 
+        echo "<br><center><h3>sube un archivo con un formato permitido:</h3> $tipo_imagen </center>";
+        echo "<a href='servidores.php' class='btn btn-danger'>Salir</a>";
+        die();
+    
+   }
+
+
+
+
+   
+    
+}
+
+
+?>
