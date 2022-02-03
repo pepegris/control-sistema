3f5bf7af3cb59ca5262ae51e243c3c71d2e0f948
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-02-03 15:40:06 -0400

    update

--- log/fiscales/includes/icono.php
+++ log/fiscales/includes/icono.php
@@ -4,3 +4,10 @@
     bottom: 90%;
     right: 10%;
     font-size:30px;" >Buscar Fiscales</a>
+
+
+<a href="buscador.php" class="btn btn-dark" style="position: fixed;
+    bottom: 90%;
+    right: 10%;
+    top:30%;
+    font-size:30px;" >Estado de Fiscales</a>
