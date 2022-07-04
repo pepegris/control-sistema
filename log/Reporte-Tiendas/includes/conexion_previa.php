6d5d47d0e41e31b70cd2427c93e7a89c12661ab8
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-04 13:28:06 -0400

    up

--- log/Reporte-Tiendas/includes/conexion_previa.php
+++ log/Reporte-Tiendas/includes/conexion_previa.php
@@ -1,6 +1,6 @@
 <?php
 
-            $serverName = "SQL"; 
+            $serverName = "172.16.1.19"; 
             $connectionInfo = array( "Database"=>"PREVIA_A", "UID"=>"mezcla", "PWD"=>"Zeus33$");
             $conn_sql = sqlsrv_connect( $serverName, $connectionInfo);
 if (!$conn) {
