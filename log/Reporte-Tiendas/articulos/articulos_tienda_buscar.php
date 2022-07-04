4e369c790218c18b4506b4be8441a6ad008dcbb2
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-04 11:32:22 -0400

    update

--- log/Reporte-Tiendas/articulos/articulos_tienda_buscar.php
+++ log/Reporte-Tiendas/articulos/articulos_tienda_buscar.php
@@ -25,8 +25,8 @@ include '../includes/cabecera.php';
 
   require '../includes/conexion_previa.php';
 
-    $sql = "SELECT co_lin from art  group by co_lin ";
-    $consulta= sqlsrv_query($conn,$sql);
+    $sql_co_lin = "SELECT co_lin from art  group by co_lin ";
+    $consulta_co_lin= sqlsrv_query($conn,$sql_co_lin);
          
 ?>
 
@@ -75,7 +75,7 @@ include '../includes/cabecera.php';
       
 
 
-    while ($row=sqlsrv_fetch_array($consulta)) {
+    while ($row=sqlsrv_fetch_array($consulta_co_lin)) {
       $co_lin=$row['co_lin'];
  
       ?>
