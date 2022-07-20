39b2294b76b419c37ef7f9214c7bf9e43c1eaae6
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 11:02:34 -0400

    A

--- Jorinacha-Report/services/mysql.php
+++ Jorinacha-Report/services/mysql.php
@@ -9,14 +9,14 @@ class Tiendas
     private $conn;
     private $conn2 = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
 
-    private function __construct()
+    public function __construct()
     {
 
         try {
 
             $this->conn= new PDO($this->inf, $this->username, $this->password );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+            echo "CONEXION";
         } catch (PDOException $err) {
 
             return "Falla de Conexion $err->getMessage()";
