79216b0dfe21cfb6e04af2aea3d97dac58cbb224
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 11:28:28 -0400

    a

--- Jorinacha-Report/services/mysql.php
+++ Jorinacha-Report/services/mysql.php
@@ -1,35 +1,17 @@
 <?php 
 echo "incio";
-class Mysql{
-
-    private $servername = "localhost";
-    private $database = "control_sistema";
-    private $username = "root";
-    private $password = "";
-    // Create connection
-    private $conna = mysqli_connect($servername, $username, $password, $database); 
-
-    public function __construct()
-    {
-        try {
-
-            $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
-            if (!$conn) {
+$servername = "localhost";
+$database = "control_sistema";
+$username = "root";
+$password = "";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
     
-                die("Connection failed: " . mysqli_connect_error());
-            }
-            
-        } catch (Exception $e) {
-            echo "$e";
-        }
-
-        
-    }
-
-
-
-
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 
 /* $conexion= new Mysql();
 $this->conexion->conexion(); */
