8041d4bb1f8fb31c824c39fa10ffece664a5a44b
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 11:18:53 -0400

    a

--- Jorinacha-Report/services/mysql.php
+++ Jorinacha-Report/services/mysql.php
@@ -1,53 +1,32 @@
-<?php
-class Tiendas
-{
+<?php 
+
+class Mysql{
+
     private $servername = "localhost";
     private $database = "control_sistema";
     private $username = "root";
     private $password = "";
-    private $conn;
-    private $conn2 = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+    // Create connection
+    private $conna = mysqli_connect($servername, $username, $password, $database); 
 
-    public function __construct()
-    {
+    function conexion(){
 
-        $res=$this->conn2;
+        try {
 
-        if (!$res) {
-    
-            die("Connection failed: " . mysqli_connect_error());
+            $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+            return $conn;
+            
+        } catch (mysqli_sql_exception $e) {
+            echo "$e";
         }
 
 
-/*         $inf = "mysql:host=$this->servername;dbname=$this->database;charset=utf8"; 
-        try {
 
-            $this->conn= new PDO($inf, $this->username, $this->password );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "CONEXION";
-        } catch (PDOException $err) {
-
-            return "Falla de Conexion $err->getMessage()";
-
-        } */
-        
     }
 
-/*     public function getTiendas()
-    {
 
-        try {
 
-            $conexion=$this->Conexion();
-            $res = $conexion->query("SELECT sedes_nom FROM sedes WHERE    estado_sede <> 'inactivo'");
-            return $res;
-
-        } catch (PDOException $err) {
-            return "Falla de Conexion $err->getMessage()";
-        }
-    } */
 }
-
 
 
 ?>
--- Jorinacha-Report/src/art-stock/form.php
+++ Jorinacha-Report/src/art-stock/form.php
@@ -1,21 +1,8 @@
 <?php
 require '../../includes/log.php';
 include '../../includes/header.php';
-require '../../services/mysql.php';
-/* $service=new Tiendas();
-var_dump($service); */
-/* $res=$service->getTiendas(); */
-/* $servername = "localhost";
-$database = "control_sistema";
-$username = "root";
-$password = "";
+include '../../services/mysql.php';
 
-
-$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password );
-$res = $conn->query("SELECT sedes_nom FROM sedes WHERE    estado_sede <> 'inactivo'");*/
-/* foreach ($res as $key ) {
-  var_dump($key['sedes_nom']);
-}  */
 ?>
 
 <div id="body">
