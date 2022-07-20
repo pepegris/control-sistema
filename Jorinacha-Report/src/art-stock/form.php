f7ebe946f02c17c738190877715633ae3cc2dfe9
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 14:51:15 -0400

    a

--- Jorinacha-Report/src/art-stock/form.php
+++ Jorinacha-Report/src/art-stock/form.php
@@ -30,13 +30,13 @@ include '../../services/sqlserver.php';
       </div>
 
       <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
         <label class="form-check-label" for="flexCheckDefault1">
           Default checkbox1
         </label>
       </div>
       <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked2" checked>
         <label class="form-check-label" for="flexCheckChecked2">
           Checked checkbox2
         </label>
