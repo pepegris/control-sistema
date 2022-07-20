a1fa23c45cd16df237b615119541c7fe0065b7f3
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 16:16:49 -0400

    a

--- Jorinacha-Report/services/sqlserver.php
+++ Jorinacha-Report/services/sqlserver.php
@@ -9,37 +9,36 @@ if (!$conn) {
 }
 
  */
-function getCat_art(){
+function getCat_art()
+{
 
-    
-    $serverName = "172.16.1.19"; 
-    $connectionInfo = array( "Database"=>"PREVIA_A", "UID"=>"mezcla", "PWD"=>"Zeus33$");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+    $serverName = "172.16.1.19";
+    $connectionInfo = array("Database" => "PREVIA_A", "UID" => "mezcla", "PWD" => "Zeus33$");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
     $sql = "SELECT co_cat,cat_des FROM cat_art ";
-    $res = sqlsrv_query($conn,$sql);
+    $res = sqlsrv_query($conn, $sql);
 
     return $res;
-
 }
 
-function getLin_art(){
+function getLin_art()
+{
 
-    
-    $serverName = "172.16.1.19"; 
-    $connectionInfo = array( "Database"=>"PREVIA_A", "UID"=>"mezcla", "PWD"=>"Zeus33$");
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+    $serverName = "172.16.1.19";
+    $connectionInfo = array("Database" => "PREVIA_A", "UID" => "mezcla", "PWD" => "Zeus33$");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
     $sql = "SELECT co_lin,lin_des from lin_art";
-    $res = sqlsrv_query($conn,$sql);
+    $res = sqlsrv_query($conn, $sql);
 
-    
+
     while ($row2 = sqlsrv_fetch_array($res)) {
-
-    $co_lin []= $row2[co_lin]; 
-    $co_lin []= $row2[lin_des]; 
-
-}
+        $i=0;
+        $co_lin[$i] = $row2[co_lin];
+        $co_lin[$i] = $row2[lin_des];
+    }
 
 
     return $co_lin;
-
 }
