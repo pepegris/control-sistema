d22c660949df47dfaca691951fe6fce44a6220f1
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 08:58:38 -0400

    a

--- Jorinacha-Report/includes/footer.php
+++ Jorinacha-Report/includes/footer.php
@@ -1,17 +1,20 @@
 <footer id="footer">
-	<center>
-		<div class="inner">
-			<h4>Slack</h4>
-			<ul class="icons">
-				<li><a href="https://inv-jorinacha.slack.com" target="_blank" class="icon brands alt fa-slack"><span class="label">Slack Jorinacha</span></a></li>
-			</ul>
+
+	<div class="inner">
+
+		<center>
 			<ul class="copyright">
 				<li>
 					<p>Inversiones Jorinacha © 2021 By Andres Salcedo | Departamento de Sistema</p>
 				</li>
 			</ul>
-		</div>
-	</center>
+		</center>
+		<h4>Slack</h4>
+		<ul class="icons">
+			<li><a href="https://inv-jorinacha.slack.com" target="_blank" class="icon brands alt fa-slack"><span class="label">Slack Jorinacha</span></a></li>
+		</ul>
+	</div>
+
 </footer>
 
 
