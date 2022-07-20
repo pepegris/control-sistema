d8f9b29e82864cd6062bca05d4335d2982b49cd6
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 14:37:42 -0400

    a

--- Jorinacha-Report/src/art-stock/form.php
+++ Jorinacha-Report/src/art-stock/form.php
@@ -15,6 +15,19 @@ include '../../services/sqlserver.php';
         <legend>Reporte</legend>
       </center>
 
+      <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Checked checkbox
+  </label>
+</div>
+
       
       <label for="sede" class="form-label ">Sedes</label>
       <div class="form-check">
