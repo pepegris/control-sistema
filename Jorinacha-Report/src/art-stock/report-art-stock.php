34a49cb4fc60dbdc6b435b55686ec5884aaf0b3e
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-20 20:17:19 -0400

    table

--- Jorinacha-Report/src/art-stock/report-art-stock.php
+++ Jorinacha-Report/src/art-stock/report-art-stock.php
@@ -6,40 +6,45 @@ include '../../services/sqlserver.php';
 
 if ($_POST) {
 
-
     
     $linea=$_POST['linea'];
     for ($i=0; $i < 20 ; $i+=1) { 
         $sedes [] = $_POST[$i];
     }
 
-    $bd = array (
-        "Previa Shop"=> 'PREVIA_A',
-        "Comercial Merina"=> 'MERINA21',
-        "Comercial Merina III"=> 'MRIA3A21',
-        "Comercial Corina I"=>'CORINA21',
-        "Comercial Corina II"=>'CORI2_21',
-        "Comercial Punto Fijo"=> 'PUFIJO21',
-        "Comercial Matur"=> 'MATURA21',
-        "Comercial Valena"=> 'VALENA21',
-        "Comercial Trina"=> 'TRAINA21',
-        "Comercial Kagu"=> 'KAGUA21',
-        "Comercial Nachari"=> 'NACHAR21',
-        "Comercial Higue"=> 'HIGUE21',
-        "Comercial Apura"=>'APURA21',
-        "Comercial Vallepa"=> 'VALLEP21',
-        "Comercial Ojena"=> 'OJENA21',
-        "Comercial Puecruz"=> 'PUECRU21',
-        "Comercial Acari"=> 'ACARI21',
-        "Comercial Catica II" => 'CATICA21',
-    );
-
-    var_dump($bd["dsadadsa"]);
 
 ?>
 
 
-
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 
 
 
