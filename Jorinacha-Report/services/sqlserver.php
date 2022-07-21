c0c5e343dfdcbab8326e6b1d1c1d6b76dba0abfe
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-21 10:02:37 -0400

    A

--- Jorinacha-Report/services/sqlserver.php
+++ Jorinacha-Report/services/sqlserver.php
@@ -88,13 +88,12 @@ function getArt($sede, $linea)
 
                 $sql = "SELECT LTRIM(RTRIM(co_art)) as  co_art  ,LTRIM(RTRIM(art_des)) as  art_des  , 
                 co_color , co_cat , co_lin , stock_act , prec_vta1 , prec_vta2 , prec_vta3 ,prec_vta4 ,prec_vta5 
-                from art
-                WHERE prec_vta5 >= 1 ";
+                from art WHERE prec_vta5 >= 1 ORDER BY stock_act DESC";
             } else {
 
                 $sql = "SELECT LTRIM(RTRIM(co_art)) as  co_art  ,LTRIM(RTRIM(art_des)) as  art_des  ,
                 co_color , co_cat , co_lin , stock_act , prec_vta1 , prec_vta2 , prec_vta3 ,prec_vta4 ,prec_vta5 
-                from art  where co_lin= '$linea' AND prec_vta5 >= 1";
+                from art  where co_lin= '$linea' AND prec_vta5 >= 1 ORDER BY stock_act DESC";
             }
 
 
