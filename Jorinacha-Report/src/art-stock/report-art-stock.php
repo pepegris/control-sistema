da49948930692c76e13b8806d69bff3b9c9035b8
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-27 12:23:38 -0400

    a

--- Jorinacha-Report/src/art-stock/report-art-stock.php
+++ Jorinacha-Report/src/art-stock/report-art-stock.php
@@ -96,7 +96,7 @@ if ($_POST) {
 
 
           ?>
-          <td><?= var_dump($res2); ?></td>
+          <td><?= var_dump($fecha1); ?></td>
           <td><?= $prec_vta1 ?></td>
           <td>$<?= $prec_vta5 ?></td>
           <td><?= $stock_act ?></td>
