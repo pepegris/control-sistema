4fa40d5b12df3d7d040ca66f6bee0a9f002ef234
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-07-27 13:45:59 -0400

    a

--- Jorinacha-Report/src/art-stock/report-art-stock.php
+++ Jorinacha-Report/src/art-stock/report-art-stock.php
@@ -11,7 +11,7 @@ if ($_POST) {
 /*   $fecha1 = $_POST['fecha1'];
   $fecha2 = $_POST['fecha2']; */
   $fecha1 = date("Ymd", strtotime($_POST['fecha1']));
-  $fecha2 = date("Ymd", strtotime($_POST['fecha3']));  
+  $fecha2 = date("Ymd", strtotime($_POST['fecha2']));  
 var_dump($fecha1);
   for ($i = 0; $i < 20; $i += 1) {
     $sedes[] = $_POST[$i];
