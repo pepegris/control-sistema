74936c9fd49feecbdd8deb080c4356d753336aaa
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-05 15:36:56 -0400

    a

--- Jorinacha-Report/src/art/report-art-stock.php
+++ Jorinacha-Report/src/art/report-art-stock.php
@@ -92,15 +92,12 @@ if ($_POST) {
               $fec_lote = $res2['fec_lote'];
               $total_art = $res2['total_art'];
                
-              foreach ($fec_lote as &$prueba){
-                $prueba=$prueba;
-              }
 
 
 
           ?>
           <td><?= $fact_num  ?></td>
-          <td><?php var_dump($prueba);  ?></td>
+          <td><?php var_dump(extract($fec_lote));  ?></td>
           <td><?= $total_art ?></td>
           <td><?= $stock_act ?></td>
           <?php
