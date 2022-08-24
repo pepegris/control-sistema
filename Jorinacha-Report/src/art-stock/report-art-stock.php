907aefa2ea37af513b0c37998a069d531c311503
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-24 10:53:20 -0400

    a

--- Jorinacha-Report/src/art-stock/report-art-stock.php
+++ Jorinacha-Report/src/art-stock/report-art-stock.php
@@ -167,7 +167,7 @@ if ($_POST) {
               $res6 = getArt($sedes[$f], $linea, $co_art);
               $prec_vta5_tienda = round($res6[$i]['prec_vta5']);
 
-              $res7 = getCotizacion($sedes[$f], $co_art);
+              $res7 = getCotizacion($sedes[$i], $co_art);
               $total_pedido = $res7['total_art'];
               $status = $res7['status'];
               $documento = 'Cot'
