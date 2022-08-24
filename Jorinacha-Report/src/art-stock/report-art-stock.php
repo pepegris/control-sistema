d55f5978ae6072807f34b891076bf574a666b36f
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-24 10:37:02 -0400

    a

--- Jorinacha-Report/src/art-stock/report-art-stock.php
+++ Jorinacha-Report/src/art-stock/report-art-stock.php
@@ -167,10 +167,30 @@ if ($_POST) {
               $res6 = getArt($sedes[$f], $linea, $co_art);
               $prec_vta5_tienda = round($res6[$i]['prec_vta5']);
 
-              $res7 = getCotizacion($sedes[$f], $co_art);
+             /*  $res7 = getCotizacion($sedes[$f], $co_art);
               $total_pedido = $res7['total_art'];
               $status = $res7['status'];
-              $documento = $res7['doc'];
+              $documento = $res7['doc']; */
+
+              $serverName = "172.16.1.19";
+              $connectionInfo = array("Database" => "PREVIA_A", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+              $conn = sqlsrv_connect($serverName, $connectionInfo);
+  
+              $sql = "SELECT top 1 cotiz_c.fact_num,reng_cac.total_art,cotiz_c.status  
+              FROM reng_cac INNER JOIN cotiz_c ON reng_cac.fact_num=cotiz_c.fact_num
+              WHERE reng_cac.co_art ='$co_art' and cotiz_c.co_cli='$cliente'
+              ORDER BY fe_us_in DESC";
+  
+              $consulta = sqlsrv_query($conn, $sql);
+  
+                  while ($row = sqlsrv_fetch_array($consulta)) {
+  
+                      $total_art['total_art'] = number_format($row['total_art'], 0, ',', '.');
+                      $total_art['status'] = $row['status'];
+                      $total_art['doc'] = 'Cot';
+                      break;
+                  }
+                  $res7 = $total_art;
 
           ?>
               <td><?= $stock_act_tienda  ?></td>
