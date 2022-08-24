58115528409a040392c0cfccbfbf6d2e21a7e223
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-24 08:13:21 -0400

    a

--- Jorinacha-Report/src/art-stock/report-art-stock copy.php
+++ Jorinacha-Report/src/art-stock/report-art-stock copy.php
@@ -60,7 +60,6 @@ form , td {
           if ($sedes[$i] != 'Previa Shop') {
             echo "<th scope='col'>Cant Env $i</th>";
             echo "<th scope='col'>Total Vendido $i</th>";
-            echo "<th scope='col'>Ref $i</th>";
           }
             
         ?>
@@ -72,24 +71,24 @@ form , td {
     <tbody>
       <?php
 
-      $res0 = getArt('Previa Shop', $linea);
+      $res1 = getArt('Previa Shop', $linea);
       
       $n = 1;
-      for ($e = 0; $e < count($res0); $e++) {
+      for ($e = 0; $e < count($res1); $e++) {
 
-        $co_art = $res0[$e]['co_art'];
-        $co_lin = getLin_art($res0[$e]['co_lin']);
-        $co_subl = getSub_lin($res0[$e]['co_subl']);
-        $co_cat = getCat_art($res0[$e]['co_cat']);
-        $co_color = getColores($res0[$e]['co_color']);
+        $co_art = $res1[$e]['co_art'];
+        $co_lin = getLin_art($res1[$e]['co_lin']);
+        $co_subl = getSub_lin($res1[$e]['co_subl']);
+        $co_cat = getCat_art($res1[$e]['co_cat']);
+        $co_color = getColores($res1[$e]['co_color']);
 
-        $stock_act = round($res0[$e]['stock_act']);
+        $stock_act = round($res1[$e]['stock_act']);
         $total_stock_act_previa += $stock_act;
 
-        $precio = round($res0[$e]['prec_vta1']);
+        $precio = round($res1[$e]['prec_vta1']);
         $prec_vta1 = number_format($precio, 2, ',', '.');
 
-        $prec_vta5 = round($res0[$e]['prec_vta5']);
+        $prec_vta5 = round($res1[$e]['prec_vta5']);
 
       ?>
 
@@ -107,12 +106,11 @@ form , td {
 
             if ($sedes[$g] != null) {
 
-              $res1 = getReng_fac($sedes[$g],  $co_art, $fecha1, $fecha2);
-              $total_vendido += round($res1);
+              $res2 = getReng_fac($sedes[$g],  $co_art, $fecha1, $fecha2);
+              $total_vendido += round($res2);
 
-              $res2= getCompras($co_art);
-              $prec_vta = $res2['prec_vta'];
-
+              $res0= getCompras($co_art);
+              $prec_vta = $res0['prec_vta'];
 
             }
             $g++;
@@ -137,23 +135,19 @@ form , td {
               $res3 = getArt_stock_tiendas($sedes[$f], $co_art);
               $stock_act_tienda = round($res3[0]['stock_act']);
               $total_stock_act_tienda[$sedes[$f]] += $stock_act_tienda;
-           
-              $res4 = getFactura($sedes[$f], $co_art,$fecha1, $fecha2);
-              $total_enviado = round($res4['total_art']);
-              $total_enviado_tienda[$sedes[$f]] += $total_enviado;
 
-              $res5 = getReng_fac($sedes[$f],  $co_art, $fecha1, $fecha2);
-              $vendido_tienda = round($res5);
+              $res4 = getReng_fac($sedes[$f],  $co_art, $fecha1, $fecha2);
+              $vendido_tienda = round($res4);
               $total_vendido_tienda [$sedes[$f]] += $vendido_tienda;
 
-              $res6= getArt($sedes[$g],$co_art);
-              $prec_vta5_tienda = round($res6[$e]['prec_vta5']);
+              $res5 = getFactura($sedes[$f], $co_art,$fecha1, $fecha2);
+              $total_enviado = round($res5['total_art']);
+              $total_enviado_tienda[$sedes[$f]] += $total_enviado;
 
           ?>
               <td><?= $stock_act_tienda  ?></td>
               <td><?= $total_enviado  ?></td>
               <td><?= $vendido_tienda ?></td>
-              <td><?= $prec_vta5_tienda ?></td>
 
           <?php $f++;
             }
@@ -182,7 +176,6 @@ form , td {
           <td><?= $total_stock_act_tienda[$sedes[$h]] ?></td>
           <td><?= $total_enviado_tienda[$sedes[$h]] ?></td>
           <td><?= $vendido  ?></td>
-          <td></td>
 
           <?php
 
