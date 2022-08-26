35460381ab5e16050a8ece709b514d851d0e7964
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-26 10:36:34 -0400

    a

--- Jorinacha-Report/src/ordenes/prueba.php
+++ Jorinacha-Report/src/ordenes/prueba.php
@@ -13,10 +13,8 @@ $Day = date("d", strtotime($fecha2));
 $Month = date("m", strtotime($fecha2));
 $Year = date("Y", strtotime($fecha2));
 
-$a=strval($Day);
-$b=strval($Month);
-$c=strval($Year);
-$fecha = $a+ $b + $c;
+
+$fecha = "$Day"  + "$Month" + "$Year";
 echo $fecha ;
 for ($i = 0; $i < 20; $i += 1) {
     $sedes[] = $_POST[$i];
