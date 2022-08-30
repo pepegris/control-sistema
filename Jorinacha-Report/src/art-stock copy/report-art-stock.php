ad7f113c10d642c35bb92dd2616314cf9a571ca6
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-30 10:11:23 -0400

    a

--- Jorinacha-Report/src/art-stock copy/report-art-stock.php
+++ Jorinacha-Report/src/art-stock copy/report-art-stock.php
@@ -12,7 +12,7 @@ if (isset($_GET)) {
 
   $linea = $_GET['linea'];
   $sedes= $_GET['sedes'];
-  var_dump($linea);
+  var_dump($sedes);
 /*   $fecha1 = $_GET['fecha1'];
   $fecha2 = $_GET['fecha2']; */
   $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
