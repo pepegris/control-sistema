002aa2816740927914afdcbf2b1948bd3920793d
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-08-30 09:42:19 -0400

    Update routes.php

--- Jorinacha-Report/src/art-stock copy/routes.php
+++ Jorinacha-Report/src/art-stock copy/routes.php
@@ -5,16 +5,17 @@ if (isset($_POST)) {
     include '../../includes/loading.php';
 
     $pedidos=$_POST['pedidos'];
-
+    var_dump($pedidos);
     if ($pedidos=='con') {
-        header('refresh:5;url= report-art-stock-completo.php');
+        
+        #header('refresh:5;url= report-art-stock-completo.php');
     }elseif ($pedidos=='sin') {
-        header('refresh:5;url= report-art-stock.php');
+        #header('refresh:5;url= report-art-stock.php');
     }else {
-        header('refresh:5;url= form.php');
+        #header('refresh:5;url= form.php');
     }
 
 } else {
-    header('refresh:5;url= form.php');
+    #header('refresh:5;url= form.php');
     exit;
 }
