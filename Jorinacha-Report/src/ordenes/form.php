188e464b3956868a78a407934bb0e44aca6f80e4
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-09-06 13:38:10 -0400

    Update form.php

--- Jorinacha-Report/src/ordenes/form.php
+++ Jorinacha-Report/src/ordenes/form.php
@@ -14,13 +14,17 @@ include '../../services/sqlserver.php';
     display: flexbox;
 
   }
-  h1{
+
+  h1 {
     display: block;
   }
 </style>
-
+<center>
+  <h1>Reporte de Ordenes de Pagos</h1>
+</center>
 <div id="body">
-  <center><h1>Reporte de Ordenes de Pagos</h1></center>
+
+
   <form action="report-art-stock.php" method="POST">
 
     <div class="fieldset">
@@ -28,27 +32,28 @@ include '../../services/sqlserver.php';
       <center>
         <legend>Reporte</legend>
       </center>
-      
-      
+
+
       <div class="form-check">
         <?php
         $res1 = getTiendas();
         $i = 0;
-        while ($row1= mysqli_fetch_array($res1)) {
+        while ($row1 = mysqli_fetch_array($res1)) {
 
           $sede = $row1['sedes_nom'];
-          if ($sede=='Sede Boleita') {
+          if ($sede == 'Sede Boleita') {
             $sede = 'Previa Shop';
           }
-          
+
         ?>
 
-          <input class="form-check-input" type="checkbox" value="<?= $sede ?>" name="<?= $i ?>"  checked>
+          <input class="form-check-input" type="checkbox" value="<?= $sede ?>" name="<?= $i ?>" checked>
           <label class="form-check-label" for="<?= $sede ?>">
             <?= $sede ?>
           </label>
 
-        <?php  $i ++; } ?>
+        <?php $i++;
+        } ?>
 
       </div>
 
@@ -57,23 +62,23 @@ include '../../services/sqlserver.php';
         <label for="ordenes" class="form-label ">Ordenes de:</label>
         <select name="ordenes" id="">
 
-            <option value="tiendas">Tiendas</option> 
-            <option value="bole">Bole</option> 
+          <option value="tiendas">Tiendas</option>
+          <option value="bole">Bole</option>
 
         </select>
       </div>
 
       <div class="form-group">
         <label for="fecha1" class="form-label ">Desde</label>
-          <input type="date" name="fecha1" id="">
+        <input type="date" name="fecha1" id="">
       </div>
 
       <div class="form-group">
         <label for="fecha2" class="form-label ">Hasta</label>
-          <input type="date" name="fecha2" id="">
+        <input type="date" name="fecha2" id="">
       </div>
       <br>
-      <center><button type="submit"  class="btn btn-primary">Ingresar</button></center>
+      <center><button type="submit" class="btn btn-primary">Ingresar</button></center>
       <br>
     </div>
   </form>
