5dce849084ad3411afc2b5a93bc06a022337a72b
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-09-06 14:02:14 -0400

    Update home.php

--- Jorinacha-Report/home.php
+++ Jorinacha-Report/home.php
@@ -43,7 +43,7 @@ $cuenta_on = ucwords($cuenta_on);
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="172.16.1.11/php/LoginJorinacha/inicio.php">Jorinacha</a>
+      <a class="navbar-brand" href="../../LoginJorinacha/inicio.php">Jorinacha</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
