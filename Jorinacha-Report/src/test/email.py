945b37bc119ae31691e93fcfd65436ae862309d5
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-09-20 14:56:21 -0400

    email

---
+++ Jorinacha-Report/src/test/email.py
@@ -0,0 +1,55 @@
+from multiprocessing import connection
+import pyodbc
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+try:
+    connection=pyodbc.connect('DRIVER={SQL Server};SERVER=172.16.1.19;DATABASE=previa_a ;UID=mezcla;PWD=Zeus33$')
+    print ("conexion exitosa \n")
+
+    cursor=connection.cursor()
+    cursor.execute("select TOP 1 tipo_doc,nro_doc,anulado,co_cli,observa  from docum_cc order by fe_us_in desc")
+    rows=cursor.fetchall()
+    inf = {}
+    
+    for row in rows:
+        tipo_doc=row[0]
+        nro_doc=row[1]
+        observa=row[4]
+
+    email_emisor = 'soporte.it.jorinacha@gmail.com'
+    email_pass ='rcnkchteocmgcaoe'
+    email_receptor ='irwingduran@hotmail.com'
+
+
+    asunto = 'PRObANDO CORREOS'
+
+
+    cuerpo= f"""
+    {email_receptor}
+    este es el correo que se va enviar \n
+    {tipo_doc}
+    {nro_doc}
+    {observa}
+    prueba de envio
+    """
+
+    em=EmailMessage()
+    em['From'] = email_emisor
+    em['To'] = email_receptor
+    em['Subject'] = asunto
+    em.set_content(cuerpo)
+
+
+    contexto =  ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com' ,465, context = contexto ) as smtp :
+        smtp.login(email_emisor , email_pass)
+        smtp.sendmail(email_emisor,email_receptor,em.as_string())
+   
+        
+except Exception as ex:
+    print(ex)
+
---
+++ Jorinacha-Report/src/test/mail.php
@@ -0,0 +1,11 @@
+
+<?php
+$para      = 'soporte.it.jorinacha@gmail.com';
+$titulo    = 'El título';
+$mensaje   = 'Hola';
+$cabeceras = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($para, $titulo, $mensaje, $cabeceras);
+?>
