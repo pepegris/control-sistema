0fab65ff4ce5b490c14c44c57b2bada2cdf412e2
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-09-21 09:52:48 -0400

    excel

--- Jorinacha-Report/src/precios/report-art-stock.php
+++ Jorinacha-Report/src/precios/report-art-stock.php
@@ -203,6 +203,43 @@ if (isset($_POST)) {
 
     </tbody>
   </table>
+
+
+  <script>
+    function htmlExcel(idTabla, nombreArchivo = '') {
+  let linkDescarga;
+  let tipoDatos = 'application/vnd.ms-excel';
+  let tablaDatos = document.getElementById(idTabla);
+  let tablaHTML = tablaDatos.outerHTML.replace(/ /g, '%20');
+
+  // Nombre del archivo
+  nombreArchivo = nombreArchivo ? nombreArchivo + '.xlsx' : 'Reporte_Puntos_Canjeados.xlsx';
+
+  // Crear el link de descarga
+  linkDescarga = document.createElement("a");
+
+  document.body.appendChild(linkDescarga);
+
+  if (navigator.msSaveOrOpenBlob) {
+    let blob = new Blob(['\ufeff', tablaHTML], {
+      type: tipoDatos
+    });
+    navigator.msSaveOrOpenBlob(blob, nombreArchivo);
+  } else {
+    // Crear el link al archivo
+    linkDescarga.href = 'data:' + tipoDatos + ', ' + tablaHTML;
+
+    // Setear el nombre de archivo
+    linkDescarga.download = nombreArchivo;
+
+    //Ejecutar la función
+    linkDescarga.click();
+  }
+}
+  </script>
+
+<button onclick="htmlExcel('tblData', 'Reporte_Puntos_Canjeados')">Exportar reporte</button>
+
   <script src="../../assets/js/excel.js"></script>
   <center>
     <button id="submitExport" class="btn btn-success">Exportar Reporte a EXCEL</button>
