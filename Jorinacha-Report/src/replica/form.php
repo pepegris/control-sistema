b58c3637919889ae839721e9fa869406bf27a44f
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-10-27 12:49:50 -0400

    Update form.php

--- Jorinacha-Report/src/replica/form.php
+++ Jorinacha-Report/src/replica/form.php
@@ -12,80 +12,86 @@ include '../../services/sqlserver.php';
 
     width: 23px;
   }
-  ul{
-  margin-top: 10px;
-}
+
+  ul {
+    margin-top: 10px;
+  }
 
 
 
-  @media (max-width: 900px){
+  @media (max-width: 900px) {
 
- ul li{
-  font-size: 10px;
- }
- img {
+    ul li {
+      font-size: 10px;
+    }
+
+    img {
 
 
-width: 19px;
-}
-ul{
-  margin-top: 10px;
-}
+      width: 19px;
+    }
 
-}
+    ul {
+      margin-top: 10px;
+    }
 
+  }
 </style>
 
 
 <div id="body">
 
   <div class="slideExpandUp">
-  <ul class="list-group" >
-    <li class="list-group-item disabled" style="background-color:black" aria-disabled="true">
-      <center><b>Replica</b></center>
+    <ul class="list-group">
+      <li class="list-group-item disabled" style="background-color:black" aria-disabled="true">
+        <center><b>Replica</b></center>
 
-    </li>
-    <?php
+      </li>
+      <?php
 
-    for ($i = 0; $i < count($sedes_ar); $i++) {
+      for ($i = 0; $i < count($sedes_ar); $i++) {
 
-      if ($sedes_ar[$i] != 'Previa Shop') {
+        if ($sedes_ar[$i] != 'Previa Shop') {
 
-        $sede = $sedes_ar[$i];
+          $sede = $sedes_ar[$i];
 
-        $res = Replica($sedes_ar[$i]);
-        $res1 = $res['fec_emis'];
-        $fecha = $res1->format('d-m-Y');
+          $res = Replica($sedes_ar[$i]);
+          $res1 = $res['fec_emis'];
+          $fecha = $res1->format('d-m-Y');
 
-        $fecha_actual = date("d-m-Y");
+          $fecha_actual = date("d-m-Y");
 
-        $fecha1 = date("d-m-Y", strtotime($fecha_actual . "- 3 day"));
-        $fecha2 = date("d-m-Y", strtotime($fecha_actual . "- 7 day"));
-
-
-        $past = new DateTime($fecha);
-        $now_1 = new DateTime($fecha1);
-        $now_2 = new DateTime($fecha2);
+          $fecha1 = date("d-m-Y", strtotime($fecha_actual . "- 3 day"));
+          $fecha2 = date("d-m-Y", strtotime($fecha_actual . "- 7 day"));
 
 
+          $past = new DateTime($fecha);
+          $now_1 = new DateTime($fecha1);
+          $now_2 = new DateTime($fecha2);
 
-        if ($past  >= $now_1) {
 
-          echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span> <img src='./img/cloud-check.svg' alt=''> </li>";
-        } elseif ($past  >= $now_2) {
-          echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span>  <img src='./img/cloud-sync.svg' alt=''> </li>";
-        } else {
-          echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span>  <img src='./img/cloud-upload.svg' alt=''> </li>";
+          if ($fecha) {
+            if ($past  >= $now_1) {
+
+              echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span> <img src='./img/cloud-check.svg' alt=''> </li>";
+            } elseif ($past  >= $now_2) {
+              echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span>  <img src='./img/cloud-sync.svg' alt=''> </li>";
+            } else {
+              echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  $fecha</span>  <img src='./img/cloud-upload.svg' alt=''> </li>";
+            }
+          } else {
+
+            echo "<li class='list-group-item'><span><b style='color:black'> $sede </b> /  SINCRONIZANDO</span>  <img src='./img/cloud-upload.svg' alt=''> </li>";
+          }
         }
       }
-    }
 
 
 
 
-    ?>
+      ?>
 
-  </ul>
+    </ul>
   </div>
 
 
