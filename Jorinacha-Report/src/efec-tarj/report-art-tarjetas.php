c8effde8e084b1037c24620dbe8306d1cc09e738
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-02 14:30:01 -0400

    Update report-art-tarjetas.php

--- Jorinacha-Report/src/efec-tarj/report-art-tarjetas.php
+++ Jorinacha-Report/src/efec-tarj/report-art-tarjetas.php
@@ -14,6 +14,8 @@ if (isset($_GET)) {
   $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
   $fecha2 = date("Ymd", strtotime($_GET['fecha2']));
   var_dump($tipo_cob);
+  var_dump($fecha1);
+  var_dump($fecha2);
 
   for ($i = 0; $i < 20; $i += 1) {
     $sedes[] = $_GET[$i];
