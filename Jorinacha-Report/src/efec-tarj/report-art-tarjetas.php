8cf84a998cd314ae6b83edf3bd74d5fc0b54b6b8
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-02 14:19:16 -0400

    Update report-art-tarjetas.php

--- Jorinacha-Report/src/efec-tarj/report-art-tarjetas.php
+++ Jorinacha-Report/src/efec-tarj/report-art-tarjetas.php
@@ -78,7 +78,7 @@ if (isset($_GET)) {
         $f++;
       }
       
-      var_dump($reng_tip[1][0]);
+      var_dump($reng_tip[$sedes_ar[$f]][0]);
 
       $e = 0;
       for ($i = 0; $i < count($sedes_ar); $i++) {
