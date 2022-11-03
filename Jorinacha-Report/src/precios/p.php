d5fac8d68996f1c3ce762d447b83bc28d83dec83
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-03 12:01:31 -0400

    Update p.php

--- Jorinacha-Report/src/precios/p.php
+++ Jorinacha-Report/src/precios/p.php
@@ -11,3 +11,28 @@ var_dump($getPedidos_t);
 var_dump($pedido_tienda);
 var_dump(getPedidos_t("Comercial Merina",  '5103624386772'));
 
+
+$serverName = "172.16.1.39";
+$connectionInfo = array("Database" => "$database", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+# $sql = "EXEC getPedidos_t ,'$cliente', '$co_art' ";
+
+$sql = "SELECT CONVERT(numeric(10,0),SUM(reng_ped.total_art)) AS  total_art
+from pedidos
+JOIN reng_ped ON pedidos.fact_num=reng_ped.fact_num
+where pedidos.anulada=0 AND pedidos.status = 0 AND pedidos.co_cli='T15' AND reng_ped.co_art='5103624386772'";
+
+$consulta = sqlsrv_query($conn, $sql);
+
+if ($consulta != null) {
+    while ($row = sqlsrv_fetch_array($consulta)) {
+
+        $reng_ped = $row['total_art'];
+        break;
+    }
+    $res = $reng_ped;
+    echo "$reng_ped";
+} else {
+    $res = 0;
+}
