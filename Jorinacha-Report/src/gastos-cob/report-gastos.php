be907a354fd9c7024fe313582b9873791bc49ab3
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-08 11:38:48 -0400

    Update report-gastos.php

--- Jorinacha-Report/src/gastos-cob/report-gastos.php
+++ Jorinacha-Report/src/gastos-cob/report-gastos.php
@@ -25,7 +25,7 @@ if (isset($_GET)) {
 
   <?php
 
-  for ($i = 2; $i < count($sedes_ar); $i++) {
+  for ($i = 0; $i < count($sedes_ar); $i++) {
 
     echo "<center><h2>" . $sedes_ar[$i] . "</h2></center>";
 
@@ -43,193 +43,224 @@ if (isset($_GET)) {
 
         $res = getOrd_pago($sede,  $fecha1, $fecha2);
 
+        if ($res  != null) {
 
-        echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
-        echo "  <table class='table table-dark table-striped' >
-        <thead>
-    
-          <tr>
-                  <th scope='col'>N°</th>
-                  <th scope='col'>Núm. de Ord</th>
-                  <th scope='col'>Núm. de Mov</th>
-                  <th scope='col'>Banco</th>
-                  <th scope='col'>Cta Egreso</th>
-                  <th scope='col'>Cta Contable</th>
-                  <th scope='col'>Descripción</th>
-                  <th scope='col'>Núm. Docum</th>
-                  <th scope='col'>Monto</th>
-                  <th scope='col'>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>";
+
+          echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
+          echo "  <table class='table table-dark table-striped' >
+          <thead>
+      
+            <tr>
+                    <th scope='col'>N°</th>
+                    <th scope='col'>Núm. de Ord</th>
+                    <th scope='col'>Núm. de Mov</th>
+                    <th scope='col'>Banco</th>
+                    <th scope='col'>Cta Egreso</th>
+                    <th scope='col'>Cta Contable</th>
+                    <th scope='col'>Descripción</th>
+                    <th scope='col'>Núm. Docum</th>
+                    <th scope='col'>Monto</th>
+                    <th scope='col'>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>";
+
+        }else {
+
+          echo "<center> No hay Información Disponible <h3>" . $consultas[$o] . "</h3></center>";
+
+        }
+
+
+
 
       }elseif ($consultas[$o] == "Documentos de Pago" ) {
 
         $res = getDocum_cp($sede,  $fecha1, $fecha2);
 
-        echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
-        echo "  <table class='table table-dark table-striped' >
-        <thead>
-    
-          <tr>
-                  <th scope='col'>N°</th>
-                  <th scope='col'>Núm. de Doc</th>
-                  <th scope='col'>Núm. Factura</th>
-                  <th scope='col'>Núm. Control</th>
-                  <th scope='col'>Proveedor</th>
-                  <th scope='col'>Cta Contable</th>
-                  <th scope='col'>Descripción</th>
-                  <th scope='col'>Monto</th>
-                  <th scope='col'>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>";
+        if ($res  != null) {
+          echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
+          echo "  <table class='table table-dark table-striped' >
+          <thead>
+      
+            <tr>
+                    <th scope='col'>N°</th>
+                    <th scope='col'>Núm. de Doc</th>
+                    <th scope='col'>Núm. Factura</th>
+                    <th scope='col'>Núm. Control</th>
+                    <th scope='col'>Proveedor</th>
+                    <th scope='col'>Cta Contable</th>
+                    <th scope='col'>Descripción</th>
+                    <th scope='col'>Monto</th>
+                    <th scope='col'>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>";
+        }else {
+
+          echo "<center> No hay Información Disponible <h3>" . $consultas[$o] . "</h3></center>";
+
+        }
+
+
+
 
       }else{
 
         $res = getMov_caj($sede,  $fecha1, $fecha2);
 
-        echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
-        echo "  <table class='table table-dark table-striped' >
-        <thead>
-    
-          <tr>
-                  <th scope='col'>N°</th>
-                  <th scope='col'>Núm. de Mov Caj</th>
-                  <th scope='col'>>Cta Egreso</th>
-                  <th scope='col'>Cta Contable</th>
-                  <th scope='col'>Descripción</th>
-                  <th scope='col'>Monto</th>
-                  <th scope='col'>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>";
+        if ($res  != null) {
+          echo "<center> $sede <h3>" . $consultas[$o] . "</h3></center>";
+          echo "  <table class='table table-dark table-striped' >
+          <thead>
+      
+            <tr>
+                    <th scope='col'>N°</th>
+                    <th scope='col'>Núm. de Mov Caj</th>
+                    <th scope='col'>>Cta Egreso</th>
+                    <th scope='col'>Cta Contable</th>
+                    <th scope='col'>Descripción</th>
+                    <th scope='col'>Monto</th>
+                    <th scope='col'>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>";
+        }else {
+
+          echo "<center> No hay Información Disponible <h3>" . $consultas[$o] . "</h3></center>";
+
+        }
+
+
       }
 
       $consulta=$consultas[$o];
       $n = 1;
       $total_monto = 0;
 
-      for ($e = 0; $e < count($res); $e++) {
+      if ($res  != null) {
 
-      if ($consulta == "Ordenes de Pago") {
+        for ($e = 0; $e < count($res); $e++) {
 
-        $ord_num = $res[$e]['ord_num'];
-        $mov_num = $res[$e]['mov_num'];
-        $cod_cta = $res[$e]['cod_cta'];
-        $des_ban = $res[$e]['des_ban'];
-
-        $cta_egre = $res[$e]['cta_egre'];
-        $cta_egre_descrip = $res[$e]['cta_egre_descrip'];
-
-        $descrip = $res[$e]['descrip'];
-        
-        $cheque = $res[$e]['cheque'];
-
-        $fecha_che = $res[$e]['fecha'];
-        $fecha = $fec_che->format('d-m-Y');
-
-        $monto = $res[$e]['monto'];
-        $total_monto += $monto;
-
-        $cuenta_contable = getCuenta_contable($sede, $ord_num ,  $fecha1, $fecha2);
-        $co_cue = $cuenta_contable['co_cue'];
-        $des_cue = $cuenta_contable['des_cue'];
-
-        echo "
-        <tr>
-        <th scope='row'>$n</th>
-        <td>$ord_num</td>
-        <td>$mov_num</td>
-
-        <td>$cod_cta - $des_ban</td>
-        <td>$cta_egre - $cta_egre_descrip</td>
-
-        <td>$co_cue - $des_cue</td>
-
-        <td>$descrip</td>
-        <td>$cheque</td>
-
-        <td>$monto</td>
-        <td>$fecha</td>
-  
-        </tr>";
-        $n++;
-
-      }elseif ($consulta == "Documentos de Pago" ) {
-
-        $nro_doc = $res[$e]['nro_doc'];
-        $nro_fact = $res[$e]['nro_fact'];
-        $n_control = $res[$e]['n_control'];
-        $co_cli = $res[$e]['co_cli'];
-        $prov_des = $res[$e]['prov_des'];
-        $observa = $res[$e]['observa'];
-
-
-        $monto_net = $res[$e]['monto_net'];
-        $total_monto += $monto_net;
-
-        $fec_emis = $res[$e]['fec_emis'];
-        $fecha = $fec_emis->format('d-m-Y');
-
-        $cuenta_contable = getCuenta_contable($sede, $nro_doc ,  $fecha1, $fecha2);
-        $co_cue = $cuenta_contable['co_cue'];
-        $des_cue = $cuenta_contable['des_cue'];
-
-        echo "
-        <tr>
-        <th scope='row'>$n</th>
-        <td>$nro_doc</td>
-        <td>$nro_fact</td>
-        <td>$n_control</td>
-  
-        <td>$co_cli - $prov_des</td>
-        <td>$co_cue - $des_cue</td>
-
-        <td>$observa</td>
-        <td>$monto_net</td>
-        <td>$fecha</td>
-  
-        </tr>";
-        $n++;
-
-      }else {
-
-        $mov_num = $res[$e]['mov_num'];
-        $descrip = $res[$e]['descrip'];
-        $cta_egre = $res[$e]['cta_egre'];
-        $cta_egre_descrip = $res[$e]['cta_egre_descrip'];
-
-        $monto_d = $res[$e]['monto_d'];
-        $total_monto += $monto_d;
-
-        $fecha_cheq = $res[$e]['fecha'];
-        $fecha = $fecha_cheq->format('d-m-Y');
-
-        $cuenta_contable = getCuenta_contable($sede, $mov_num ,  $fecha1, $fecha2);
-        $co_cue = $cuenta_contable['co_cue'];
-        $des_cue = $cuenta_contable['des_cue'];
-
-        echo "
-        <tr>
-        <th scope='row'>$n</th>
-        <td>$mov_num</td>
-
-        <td>$cta_egre - $cta_egre_descrip</td>
-        <td>$co_cue - $des_cue</td>
-        
-        <td>$descrip</td>
-        <td>$monto_d</td>
-  
-        <td>$movi</td>
-        <td>$fecha</td>
-  
-        </tr>";
-        $n++;
-
-      }
-
-
+          if ($consulta == "Ordenes de Pago") {
+    
+            $ord_num = $res[$e]['ord_num'];
+            $mov_num = $res[$e]['mov_num'];
+            $cod_cta = $res[$e]['cod_cta'];
+            $des_ban = $res[$e]['des_ban'];
+    
+            $cta_egre = $res[$e]['cta_egre'];
+            $cta_egre_descrip = $res[$e]['cta_egre_descrip'];
+    
+            $descrip = $res[$e]['descrip'];
+            
+            $cheque = $res[$e]['cheque'];
+    
+            $fecha_che = $res[$e]['fecha'];
+            $fecha = $fec_che->format('d-m-Y');
+    
+            $monto = $res[$e]['monto'];
+            $total_monto += $monto;
+    
+            $cuenta_contable = getCuenta_contable($sede, $ord_num ,  $fecha1, $fecha2);
+            $co_cue = $cuenta_contable['co_cue'];
+            $des_cue = $cuenta_contable['des_cue'];
+    
+            echo "
+            <tr>
+            <th scope='row'>$n</th>
+            <td>$ord_num</td>
+            <td>$mov_num</td>
+    
+            <td>$cod_cta - $des_ban</td>
+            <td>$cta_egre - $cta_egre_descrip</td>
+    
+            <td>$co_cue - $des_cue</td>
+    
+            <td>$descrip</td>
+            <td>$cheque</td>
+    
+            <td>$monto</td>
+            <td>$fecha</td>
+      
+            </tr>";
+            $n++;
+    
+          }elseif ($consulta == "Documentos de Pago" ) {
+    
+            $nro_doc = $res[$e]['nro_doc'];
+            $nro_fact = $res[$e]['nro_fact'];
+            $n_control = $res[$e]['n_control'];
+            $co_cli = $res[$e]['co_cli'];
+            $prov_des = $res[$e]['prov_des'];
+            $observa = $res[$e]['observa'];
+    
+    
+            $monto_net = $res[$e]['monto_net'];
+            $total_monto += $monto_net;
+    
+            $fec_emis = $res[$e]['fec_emis'];
+            $fecha = $fec_emis->format('d-m-Y');
+    
+            $cuenta_contable = getCuenta_contable($sede, $nro_doc ,  $fecha1, $fecha2);
+            $co_cue = $cuenta_contable['co_cue'];
+            $des_cue = $cuenta_contable['des_cue'];
+    
+            echo "
+            <tr>
+            <th scope='row'>$n</th>
+            <td>$nro_doc</td>
+            <td>$nro_fact</td>
+            <td>$n_control</td>
+      
+            <td>$co_cli - $prov_des</td>
+            <td>$co_cue - $des_cue</td>
+    
+            <td>$observa</td>
+            <td>$monto_net</td>
+            <td>$fecha</td>
+      
+            </tr>";
+            $n++;
+    
+          }else {
+    
+            $mov_num = $res[$e]['mov_num'];
+            $descrip = $res[$e]['descrip'];
+            $cta_egre = $res[$e]['cta_egre'];
+            $cta_egre_descrip = $res[$e]['cta_egre_descrip'];
+    
+            $monto_d = $res[$e]['monto_d'];
+            $total_monto += $monto_d;
+    
+            $fecha_cheq = $res[$e]['fecha'];
+            $fecha = $fecha_cheq->format('d-m-Y');
+    
+            $cuenta_contable = getCuenta_contable($sede, $mov_num ,  $fecha1, $fecha2);
+            $co_cue = $cuenta_contable['co_cue'];
+            $des_cue = $cuenta_contable['des_cue'];
+    
+            echo "
+            <tr>
+            <th scope='row'>$n</th>
+            <td>$mov_num</td>
+    
+            <td>$cta_egre - $cta_egre_descrip</td>
+            <td>$co_cue - $des_cue</td>
+            
+            <td>$descrip</td>
+            <td>$monto_d</td>
+      
+            <td>$movi</td>
+            <td>$fecha</td>
+      
+            </tr>";
+            $n++;
+    
+          }
+    
+    
+          }
       }
 
       if ($total_mont_doc == 0) {
