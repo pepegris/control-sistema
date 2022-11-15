9fe83a1c3a3d5351b3e18f6b2e433604f59faadc
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-15 14:50:44 -0400

    Update report-ventas-diaria.php

--- Jorinacha-Report/src/ventas/report-ventas-diaria.php
+++ Jorinacha-Report/src/ventas/report-ventas-diaria.php
@@ -9,7 +9,7 @@ include '../../services/adm/ventas/diarias.php';
 
 if ($_GET) {
 
-  $divisa=$_GET['divisa'];
+  $divisa = $_GET['divisa'];
   $fecha_titulo = date("d/m/Y", strtotime($_GET['fecha1']));
   $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
 
@@ -18,14 +18,30 @@ if ($_GET) {
 
 ?>
 
-<link rel='stylesheet' href='responm.css'>
 
-  
-  
+
+  <link rel='stylesheet' href='responm.css'>
+
+
+
 
   <center>
     <h1>Ventas Diarias <?= $fecha_titulo ?></h1>
   </center>
+
+  <?php
+
+  if ($divisa == 'dl') {
+
+    echo "<h4>En Dls</h4>";
+  } else {
+    echo "<h4>En Bs</h4>";
+  }
+
+  ?>
+
+
+
   <table class="table table-dark table-striped" id="tblData">
     <thead>
 
@@ -34,7 +50,16 @@ if ($_GET) {
       <tr>
         <th scope="col">Cod</th>
         <th scope='col'>Empresa</th>
-        
+
+        <?php
+
+        if ($divisa == 'dl') {
+
+          echo "<th scope='col'>Tasa</th>";
+        }
+
+        ?>
+
         <th scope='col'>Ventas</th>
         <th scope='col'>Pares</th>
 
@@ -59,81 +84,96 @@ if ($_GET) {
 
         if ($divisa == 'dl') {
           $tasas = getTasas($sedes_ar[$i], $fecha1, 'sin');
-          $tasa_v_tasas = $tasas['tasa_v'];
-        }else {
+
+          if ($tasas != null) {
+            $tasa_v_tasas = $tasas['tasa_v'];
+          } else {
+            $tasa_v_tasas;
+          }
+        } else {
           $tasa_v_tasas  = 1;
         }
 
         $cod = Cliente($sedes_ar[$i]);
 
         $factura = getFactura($sedes_ar[$i], $fecha1, 'sin');
-        $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas ;
+        $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas;
         $tot_neto_factura = number_format($tasa_tot_neto_factura, 2, ',', '.');
 
         $factura_ven = getFactura($sedes_ar[$i], $fecha1, 'ven');
         $total_art_factura =  number_format($factura_ven['total_art'], 0, ',', '.');
 
         $dev_cli = getDev_cli($sedes_ar[$i], $fecha1, 'sin');
-        $tasa_tot_neto_dev_cli= $dev_cli['tot_neto'] / $tasa_v_tasas ;
-        $total_art_dev_cli = number_format( $dev_cli['total_art'], 0, ',', '.');
+        $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'] / $tasa_v_tasas;
+        $total_art_dev_cli = number_format($dev_cli['total_art'], 0, ',', '.');
         $tot_neto_dev_cli = number_format($tasa_tot_neto_dev_cli, 2, ',', '.');
 
         $dep_caj = getDep_caj($sedes_ar[$i], $fecha1, 'sin');
-        $tasa_total_efec_dep_caj = $dep_caj['total_efec'] / $tasa_v_tasas ;
-        $tasa_total_tarj_dep_caj = $dep_caj['total_tarj'] / $tasa_v_tasas ;
+        $tasa_total_efec_dep_caj = $dep_caj['total_efec'] / $tasa_v_tasas;
+        $tasa_total_tarj_dep_caj = $dep_caj['total_tarj'] / $tasa_v_tasas;
         $total_efec_dep_caj = number_format($tasa_total_efec_dep_caj, 2, ',', '.');
         $total_tarj_dep_caj = number_format($tasa_total_tarj_dep_caj, 2, ',', '.');
 
         $mov_ban = getMov_ban($sedes_ar[$i], $fecha1, 'sin');
-        $tasa_monto_h_mov_ban = $mov_ban['monto_h'] / $tasa_v_tasas ;
+        $tasa_monto_h_mov_ban = $mov_ban['monto_h'] / $tasa_v_tasas;
         $monto_h_mov_ban = number_format($tasa_monto_h_mov_ban, 2, ',', '.');
 
         $ord_pago = getOrd_pago($sedes_ar[$i], $fecha1, 'sin');
-        $tasa_monto_ord_pago = $ord_pago['monto'] / $tasa_v_tasas ;
+        $tasa_monto_ord_pago = $ord_pago['monto'] / $tasa_v_tasas;
         $monto_ord_pago = number_format($tasa_monto_ord_pago, 2, ',', '.');
 
 
       ?>
-      <tr>
+        <tr>
 
-        <td><?= $cod   ?></td>
-        <td><?= $sedes_ar[$i]  ?></td>
+          <td><?= $cod   ?></td>
+          <td><?= $sedes_ar[$i]  ?></td>
 
-        <td><?= $tot_neto_factura   ?></td>
-        <td><?= $total_art_factura - $total_art_dev_cli   ?></td>
+          <?php
 
-        <td><?= $tot_neto_dev_cli   ?></td>
-        <td><?= $total_art_dev_cli  ?></td>
+          if ($divisa == 'dl') {
 
-        <td><?= $monto_h_mov_ban ?></td>
-        
+            $tasa_dia = number_format($tasa_v_tasas, 2, ',', '.');
 
-        <td><?= $total_efec_dep_caj  ?></td>
-        <td><?= $total_tarj_dep_caj  ?></td>
+            echo "<td>$tasa_dia</td>";
+          }
 
-        <td><?= $monto_ord_pago  ?></td>
+          ?>
 
-      <?php
 
-      if ($monto_h_mov_ban == $tot_neto_factura ) {
-        echo "<td><i class='lni lni-checkmark-circle'></i></td>";
-      }else {
-        echo "<td><i class='lni lni-cross-circle'></i></td>";
+          <td><?= $tot_neto_factura   ?></td>
+          <td><?= $total_art_factura - $total_art_dev_cli   ?></td>
+
+          <td><?= $tot_neto_dev_cli   ?></td>
+          <td><?= $total_art_dev_cli  ?></td>
+
+          <td><?= $monto_h_mov_ban ?></td>
+
+
+          <td><?= $total_efec_dep_caj  ?></td>
+          <td><?= $total_tarj_dep_caj  ?></td>
+
+          <td><?= $monto_ord_pago  ?></td>
+
+        <?php
+
+        if ($monto_h_mov_ban == $tot_neto_factura) {
+          echo "<td><i class='lni lni-checkmark-circle'></i></td>";
+        } else {
+          echo "<td><i class='lni lni-cross-circle'></i></td>";
+        }
+
+        echo "</tr>";
       }
 
-      echo "</tr>";
-
-
-      }
-
-      ?>
+        ?>
 
 
 
-      <tr>
-        <td><b>TOTAL</b></td>
+        <tr>
+          <td><b>TOTAL</b></td>
 
-      </tr>
+        </tr>
 
     </tbody>
 
