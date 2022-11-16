c3505ffea02b94bface9f762351dc0e8f2730c02
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-16 11:12:54 -0400

    Update report-ventas-acumulado.php

--- Jorinacha-Report/src/ventas/report-ventas-acumulado.php
+++ Jorinacha-Report/src/ventas/report-ventas-acumulado.php
@@ -90,13 +90,13 @@ if ($_GET) {
 
         $cod = Cliente($sedes_ar[$i]);
 
-        $factura = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'ven');
+        $factura = getFactura($sedes_ar[$i], $fecha1, $fecha2, '');
         $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas;
 
         $factura_ven = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
         $total_art_factura =  number_format($factura_ven['total_art'], 0, ',', '.');
 
-        $dev_cli = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, 'ven');
+        $dev_cli = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, '');
         $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'] / $tasa_v_tasas;
         $tot_neto_dev_cli = number_format($tasa_tot_neto_dev_cli, 2, ',', '.');
 
@@ -108,17 +108,17 @@ if ($_GET) {
         $dev_cli_ven = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
         $total_art_dev_cli = number_format($dev_cli_ven['total_art'], 0, ',', '.');
 
-        $dep_caj = getDep_caj($sedes_ar[$i], $fecha1, $fecha2, 'ven');
+        $dep_caj = getDep_caj($sedes_ar[$i], $fecha1, $fecha2, '');
         $tasa_total_efec_dep_caj = $dep_caj['total_efec'] / $tasa_v_tasas;
         $tasa_total_tarj_dep_caj = $dep_caj['total_tarj'] / $tasa_v_tasas;
         $total_efec_dep_caj = number_format($tasa_total_efec_dep_caj, 2, ',', '.');
         $total_tarj_dep_caj = number_format($tasa_total_tarj_dep_caj, 2, ',', '.');
 
-        $mov_ban = getMov_ban($sedes_ar[$i], $fecha1, $fecha2, 'ven');
+        $mov_ban = getMov_ban($sedes_ar[$i], $fecha1, $fecha2, '');
         $tasa_monto_h_mov_ban = $mov_ban['monto_h'] / $tasa_v_tasas;
         $monto_h_mov_ban = number_format($tasa_monto_h_mov_ban, 2, ',', '.');
 
-        $ord_pago = getOrd_pago($sedes_ar[$i], $fecha1, $fecha2, 'ven');
+        $ord_pago = getOrd_pago($sedes_ar[$i], $fecha1, $fecha2, '');
         $tasa_monto_ord_pago = $ord_pago['monto'] / $tasa_v_tasas;
         $monto_ord_pago = number_format($tasa_monto_ord_pago, 2, ',', '.');
 
