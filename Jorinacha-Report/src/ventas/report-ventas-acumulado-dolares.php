ec30f0fcfa5659d4bf3b174c69407b20c83555fd
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-16 11:37:11 -0400

    up

--- Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
+++ Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
@@ -16,6 +16,11 @@ if ($_GET) {
   $fecha2 = date("Ymd", strtotime($_GET['fecha2']));
 
 
+  $Day = date("d", strtotime($fecha2));
+  $Month = date("m", strtotime($fecha2));
+  $Year = date("Y", strtotime($fecha2));
+
+
 
 
 ?>
@@ -64,7 +69,7 @@ if ($_GET) {
         <th scope='col'>Tarjeta</th>
 
         <th scope='col'>Divisas</th>
-        <th scope='col'>Gastos</th>
+		<th scope='col'>Gastos</th>
 
         <th scope='col'>Cierre Caja</th>
       </tr>
@@ -74,19 +79,50 @@ if ($_GET) {
 
       <?php
 
+      
       for ($i = 1; $i < count($sedes_ar); $i++) {
 
+        /* calcular si se solicito dolares y ver q tasa tenia ese dia */
+        if ($divisa == 'dl') {
+          $tasas = getTasas($sedes_ar[$i], $fecha1);
 
+          if ($tasas != null) {
+            $tasa_v_tasas = $tasas['tasa_v'];
+          } else {
+            $tasa_v_tasas;
+          }
+        } else {
+          $tasa_v_tasas  = 1;
+        }
 
+        $e = 1;
         $cod = Cliente($sedes_ar[$i]);
 
+        for ($r = 0; $r <= $Day; $r++) {
+
+          if ($e  < 10) {
+
+              $d = 0 . $e ;
+          } else {
+              $d = $e ;
+          }
+          
+          $fecha =  $Year . $Month . $d;
+          $res = getTasa( $fecha);
+          $tasa = number_format($res['tasa_v'], 2, ',', '.');
+
+          echo "<th>$tasa</th>";
+
+          $e++;
+      }
+
         $factura = getFactura($sedes_ar[$i], $fecha1, $fecha2, '');
-        $tasa_tot_neto_factura = $factura['tot_neto'];
+        $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas;
 
 
 
         $dev_cli = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, '');
-        $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'];
+        $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'] / $tasa_v_tasas;
         $tot_neto_dev_cli = number_format($tasa_tot_neto_dev_cli, 2, ',', '.');
 
 
@@ -97,28 +133,28 @@ if ($_GET) {
         $dev_cli_ven = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
         $total_art_dev_cli = number_format($dev_cli_ven['total_art'], 0, ',', '.');
 
-        $factura_ven = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
+		    $factura_ven = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
 
-        $venta_art = $factura_ven['total_art'] - $dev_cli_ven['total_art'];
+		    $venta_art = $factura_ven['total_art'] - $dev_cli_ven['total_art'];
         $total_art_factura  = number_format($venta_art, 0, ',', '.');
 
 
         $dep_caj = getDep_caj($sedes_ar[$i], $fecha1, $fecha2, '');
-        $tasa_total_efec_dep_caj = $dep_caj['total_efec'];
-        $tasa_total_tarj_dep_caj = $dep_caj['total_tarj'];
+        $tasa_total_efec_dep_caj = $dep_caj['total_efec'] / $tasa_v_tasas;
+        $tasa_total_tarj_dep_caj = $dep_caj['total_tarj'] / $tasa_v_tasas;
         $total_efec_dep_caj = number_format($tasa_total_efec_dep_caj, 2, ',', '.');
         $total_tarj_dep_caj = number_format($tasa_total_tarj_dep_caj, 2, ',', '.');
 
         $mov_ban = getMov_ban($sedes_ar[$i], $fecha1, $fecha2, '');
-        $tasa_monto_h_mov_ban = $mov_ban['monto_h'];
+        $tasa_monto_h_mov_ban = $mov_ban['monto_h'] / $tasa_v_tasas;
         $monto_h_mov_ban = number_format($tasa_monto_h_mov_ban, 2, ',', '.');
 
         $ord_pago = getOrd_pago($sedes_ar[$i], $fecha1, $fecha2, '');
-        $tasa_monto_ord_pago = $ord_pago['monto'];
+        $tasa_monto_ord_pago = $ord_pago['monto'] / $tasa_v_tasas;
         $monto_ord_pago = number_format($tasa_monto_ord_pago, 2, ',', '.');
 
         $ord_pago_ven = getOrd_pago($sedes_ar[$i], $fecha1, $fecha2, 'ven2');
-        $tasa_monto_ord_pago_ven = $ord_pago_ven['monto'];
+        $tasa_monto_ord_pago_ven = $ord_pago_ven['monto'] / $tasa_v_tasas;
         $monto_ord_pago_ven = number_format($tasa_monto_ord_pago_ven, 2, ',', '.');
 
 
@@ -171,14 +207,19 @@ if ($_GET) {
         }
 
         echo "</tr>";
+
       }
 
       if ($divisa == 'dl') {
 
-        $simb = '$';
-      } else {
+        $simb='$';
 
-        $simb = 'Bs';
+
+      }else {
+
+        $simb='Bs';
+
+
       }
 
         ?>
@@ -205,7 +246,7 @@ if ($_GET) {
 
           <td><b><?= $simb ?><?= number_format($total_pagos, 2, ',', '.')  ?></b></td>
           <td><b><?= $simb ?><?= number_format($total_gastos, 2, ',', '.')  ?></b></td>
-
+		  
           <td></td>
 
         </tr>
