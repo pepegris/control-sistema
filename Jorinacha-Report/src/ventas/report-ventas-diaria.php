3bc182c564047c77c4a53c3a0cc3c298adc530ef
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-16 08:59:04 -0400

    Update report-ventas-diaria.php

--- Jorinacha-Report/src/ventas/report-ventas-diaria.php
+++ Jorinacha-Report/src/ventas/report-ventas-diaria.php
@@ -100,10 +100,6 @@ if ($_GET) {
         $factura = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'sin');
         $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas;
 
-        var_dump($factura);
-        var_dump($tasa_tot_neto_factura);
-
-
         $factura_ven = getFactura($sedes_ar[$i], $fecha1, $fecha2, 'ven');
         $total_art_factura =  number_format($factura_ven['total_art'], 0, ',', '.');
 
@@ -111,8 +107,18 @@ if ($_GET) {
         $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'] / $tasa_v_tasas;
         $tot_neto_dev_cli = number_format($tasa_tot_neto_dev_cli, 2, ',', '.');
 
-        $venta += $tasa_tot_neto_factura - $tasa_tot_neto_dev_cli;
-        $tot_neto_factura = number_format($venta, 2, ',', '.');
+        if ($factura['tot_neto']  !=null) {
+
+          $venta += $tasa_tot_neto_factura - $tasa_tot_neto_dev_cli;
+          $tot_neto_factura = number_format($venta, 2, ',', '.');
+
+        }else {
+          
+          $tot_neto_factura = number_format(0, 2, ',', '.');
+        }
+
+        
+        
 
 
         $dev_cli_ven = getDev_cli($sedes_ar[$i], $fecha1, $fecha2, 'ven');
