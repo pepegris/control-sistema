0afa60307adaa23a9503532c52825ad872316862
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-11-16 08:42:38 -0400

    Update report-ventas-diaria.php

--- Jorinacha-Report/src/ventas/report-ventas-diaria.php
+++ Jorinacha-Report/src/ventas/report-ventas-diaria.php
@@ -99,7 +99,7 @@ if ($_GET) {
 
         $factura = getFactura($sedes_ar[$i], $fecha1,$fecha2, 'sin');
         $tasa_tot_neto_factura = $factura['tot_neto'] / $tasa_v_tasas;
-        $tot_neto_factura = number_format($tasa_tot_neto_factura, 2, ',', '.');
+       
 
         $factura_ven = getFactura($sedes_ar[$i], $fecha1,$fecha2, 'ven');
         $total_art_factura =  number_format($factura_ven['total_art'], 0, ',', '.');
@@ -107,6 +107,10 @@ if ($_GET) {
         $dev_cli = getDev_cli($sedes_ar[$i], $fecha1,$fecha2, 'sin');
         $tasa_tot_neto_dev_cli = $dev_cli['tot_neto'] / $tasa_v_tasas;
         $tot_neto_dev_cli = number_format($tasa_tot_neto_dev_cli, 2, ',', '.');
+
+        $venta += $tasa_tot_neto_factura - $tasa_tot_neto_dev_cli;
+        $tot_neto_factura = number_format($venta, 2, ',', '.');
+
 
         $dev_cli_ven = getDev_cli($sedes_ar[$i], $fecha1,$fecha2, 'ven');
         $total_art_dev_cli = number_format($dev_cli_ven['total_art'], 0, ',', '.');
@@ -127,7 +131,7 @@ if ($_GET) {
 
         $ord_pago_ven = getOrd_pago($sedes_ar[$i], $fecha1,$fecha2, 'ven');
         $tasa_monto_ord_pago_ven = $ord_pago_ven['monto'] / $tasa_v_tasas;
-        $monto_ord_pago_ven = number_format($tasa_monto_ord_pago, 2, ',', '.');
+        $monto_ord_pago_ven = number_format($tasa_monto_ord_pago_ven, 2, ',', '.');
 
 
         /* totales */
@@ -166,7 +170,7 @@ if ($_GET) {
           ?>
 
 
-          <td><?= $tot_neto_factura - $tot_neto_dev_cli    ?></td>
+          <td><?= $tot_neto_factura    ?></td>
           <td><?= $total_art_factura - $total_art_dev_cli   ?></td>
 
           <td><?= $tot_neto_dev_cli   ?></td>
