ef59fc9b93e78b09442c0dd15584808d8b07959c
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-12-07 13:18:14 -0400

    Update report-art-stock-fallas.php

--- Jorinacha-Report/src/art-stock/report-art-stock-fallas.php
+++ Jorinacha-Report/src/art-stock/report-art-stock-fallas.php
@@ -11,7 +11,7 @@ if (isset($_GET)) {
 
 
   $linea = $_GET['linea'];
-  $almacen=$_POST['almacen'];
+  $almacen =$_GET['almacen'];
 
   $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
   $fecha2 = date("Ymd", strtotime($_GET['fecha2']));  
@@ -73,7 +73,7 @@ form , td {
     <tbody>
       <?php
 
-      $res0 = getArt('Previa Shop', $linea,0 ,0 );
+      $res0 = getArt('Previa Shop', $linea,0 ,$almacen );
       
       $n = 1;
       for ($e = 0; $e < count($res0); $e++) {
@@ -93,19 +93,7 @@ form , td {
         $pedido = $test1['total_art'];
 
 
-        $res_stock = getArt('Previa Shop', $linea,0 ,'BOLE' );
-
-        var_dump( $res_stock );
-
-        if ($res_stock != null) {
-          $stock_act_1 = round($res_stock[0]['stock_act']);
-          var_dump( $stock_act_1 );
-        } else {
-          $stock_act_1 = 0;
-        }
-        
-
-        
+        $stock_act_1 = round($res0[$e]['stock_act']);
         $stock_act =  $stock_act_1 - $pedido; 
         $total_stock_act_previa += $stock_act;
 
