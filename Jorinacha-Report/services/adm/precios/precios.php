16f939417ac5d8aadd7662414a34b83c709dbce8
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-12-07 15:38:29 -0400

    UP

--- Jorinacha-Report/services/adm/precios/precios.php
+++ Jorinacha-Report/services/adm/precios/precios.php
@@ -163,7 +163,7 @@ function getArt($sede, $linea, $co_art, $almacen)
                 JOIN sub_lin on art.co_subl = sub_lin.co_subl
                 JOIN cat_art on art.co_cat=cat_art.co_cat
                 JOIN colores on art.co_color=colores.co_col
-                where art.co_lin='$linea' AND art.prec_vta5 >= 1 AND art.fe_us_in >= '20180101'
+                where art.co_lin='$linea' AND art.prec_vta5 >= 1 AND art.fe_us_in >= '20170101'
                 order by art.co_subl   desc";
                 
 
