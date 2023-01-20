46bba23d339030088196c8913287471ee5fb2247
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2023-01-20 12:01:05 -0400

    ordenes

--- Jorinacha-Report/services/adm/ventas/diarias.php
+++ Jorinacha-Report/services/adm/ventas/diarias.php
@@ -383,7 +383,7 @@ function getOrd_pago($sede, $fecha1, $fecha2, $data)
                 JOIN benefici ON benefici.cod_ben = ord_pago.cod_ben
                 WHERE fecha ='$fecha1' AND anulada = 0   AND ord_num < 6000000 AND cta_egre ='878'";
 
-            } elseif ($data == 'ven') {
+            }elseif ($data == 'ven') {
 
                $sql = "SELECT SUM(monto) as monto from ord_pago
                 JOIN benefici ON benefici.cod_ben = ord_pago.cod_ben
@@ -394,8 +394,14 @@ function getOrd_pago($sede, $fecha1, $fecha2, $data)
                 $sql = "SELECT SUM(monto) as monto from ord_pago
                  JOIN benefici ON benefici.cod_ben = ord_pago.cod_ben
                  WHERE fecha BETWEEN '$fecha1' AND '$fecha2'  AND anulada = 0 AND benefici.ben_des<>'PREVIA SHOP' AND cta_egre <>'878' AND ord_num < 6000000";
-             }
-            else {
+                 
+             }elseif ($data == 'ordenes') {
+
+                $sql = "SELECT fecha,ord_num,descrip,monto as monto from ord_pago
+                 JOIN benefici ON benefici.cod_ben = ord_pago.cod_ben
+                 WHERE fecha ='$fecha1' AND anulada = 0   AND ord_num < 6000000 AND cta_egre ='878'";
+ 
+             }else {
 
                 $sql = "SELECT SUM(monto) as monto from ord_pago
                 JOIN benefici ON benefici.cod_ben = ord_pago.cod_ben
