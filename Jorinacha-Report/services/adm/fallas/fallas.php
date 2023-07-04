5d7a82f2dc8438ae964499941782ee21a68d5412
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2023-07-04 15:20:12 -0400

    up

--- Jorinacha-Report/services/adm/fallas/fallas.php
+++ Jorinacha-Report/services/adm/fallas/fallas.php
@@ -28,7 +28,7 @@ $sedes_ar = array(
 
 
 
-function Database2($sede)
+function Database($sede)
 {
 
     $bd = array(
@@ -54,7 +54,7 @@ function Database2($sede)
 
     return $bd[$sede];
 }
-function Database($sede)
+function Database2($sede)
 {
 
     $bd = array(
