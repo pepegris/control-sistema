de63fcbd5e14b5851a257c9d235bee201c9b29fc
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2023-10-06 10:29:14 -0400

    Update dev.php

--- Jorinacha-Report/services/adm/devolucion/dev.php
+++ Jorinacha-Report/services/adm/devolucion/dev.php
@@ -35,7 +35,7 @@ function getLin_art_all()
 
 
 
-function getDev_cli($sede, $fecha1, $fecha2 , $art)
+function getDev_cli($sede, $fecha1, $fecha2 , $art, $lin_art)
 {
 
 
@@ -85,9 +85,7 @@ function getDev_cli($sede, $fecha1, $fecha2 , $art)
                 JOIN cat_art ON art.co_cat=cat_art.co_cat
                 JOIN colores ON art.co_color=colores.co_col
                 WHERE dev_cli.fec_emis BETWEEN '$fecha1' AND '$fecha2' AND reng_dvc.co_art='$art' AND dev_cli.anulada =0";
-            }
-
-            else {
+            }elseif ($art != '' && $lin_art !='' ){
 
 
 
@@ -106,7 +104,24 @@ function getDev_cli($sede, $fecha1, $fecha2 , $art)
                 JOIN sub_lin ON art.co_subl = sub_lin.co_subl
                 JOIN cat_art ON art.co_cat=cat_art.co_cat
                 JOIN colores ON art.co_color=colores.co_col
-                WHERE dev_cli.fec_emis BETWEEN '$fecha1' AND '$fecha2' AND reng_dvc.co_art='$art' AND dev_cli.anulada =0";
+                WHERE dev_cli.fec_emis BETWEEN '$fecha1' AND '$fecha2' AND reng_dvc.co_art='$art' AND lin_art.lin_des ='$lin_art' AND dev_cli.anulada =0";
+            }else {
+                $sql = "SELECT  art.co_art, sub_lin.subl_des , cat_art.cat_des , colores.des_col, lin_art.lin_des , art.ubicacion, art.stock_act , 
+
+                dev_cli.fact_num as dev_cli_fact,
+                dev_cli.comentario as dev_cli_comentario ,
+                dev_cli.fec_emis as dev_cli_fec_emis , 
+                reng_dvc.total_art as reng_dvc_total_art
+
+                FROM dev_cli 
+                JOIN reng_dvc ON dev_cli.fact_num = reng_dvc.fact_num
+                JOIN art ON art.co_art = reng_dvc.co_art
+
+                JOIN lin_art ON art.co_lin = lin_art.co_lin
+                JOIN sub_lin ON art.co_subl = sub_lin.co_subl
+                JOIN cat_art ON art.co_cat=cat_art.co_cat
+                JOIN colores ON art.co_color=colores.co_col
+                WHERE dev_cli.fec_emis BETWEEN '$fecha1' AND '$fecha2' AND lin_art.lin_des ='$lin_art' AND dev_cli.anulada =0";
             }
 
 
