c3dcb38ebae9c7043863614022181e5113d20a21
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-01-23 13:24:26 -0400

    Update report-ventas-diaria.php

--- Jorinacha-Report/src/ventas/report-ventas-diaria.php
+++ Jorinacha-Report/src/ventas/report-ventas-diaria.php
@@ -21,7 +21,7 @@ if ($_GET) {
 
 
 
-  $cantidadDias2 = cal_days_in_month(CAL_GREGORIAN, 02, 2023);
+  $cantidadDias = cal_days_in_month(CAL_GREGORIAN, 02, 2023);
   echo $cantidadDias;
 
   $cantidadDias2 = cal_days_in_month(CAL_GREGORIAN, 02, 2024);
