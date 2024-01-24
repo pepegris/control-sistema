edd7e75c472e601c55843ccfeed95ef73d3e6e6f
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-01-24 11:16:13 -0400

    Update report-ventas-acumulado-dolares.php

--- Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
+++ Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
@@ -134,19 +134,28 @@ if ($_GET) {
           }
 
 
-          if ($sede == "Sucursal Caracas I" && $Month <= 04 && $d <= 13 && $Year <= 2023) {
+          if ($sede == "Sucursal Caracas I" && $Month < 04  && $Year <= 2023) {
             $sede = 'Comercial Merina';
             
-
-          }elseif ($sede == "Sucursal Caracas II" && $Month <= 04 && $d <= 20 && $Year <= 2023) {
+          }elseif ( $sede == "Sucursal Caracas I" && $Month == 04 && $d < 13 && $Year <= 2023){
+            $sede = 'Comercial Merina';
+          }
+          
+          
+          if ($sede == "Sucursal Caracas II" && $Month < 04  && $Year <= 2023) {
             $sede = 'Comercial Merina3';
-            
 
-          }elseif ($sede == "Sucursal Maturin" && $Month <= 10 && $d <= 01 && $Year <= 2023) {
+          }elseif ( $sede == "Sucursal Caracas II" && $Month == 04   && $d < 20 && $Year <= 2023){
+            $sede = 'Comercial Merina3';
+          }
+          
+          if ($sede == "Sucursal Maturin" && $Month < 10  && $Year <= 2023) {
             $sede = 'Comercial Matur';
-            
-
-          }elseif ($sede == "Sucursal Cagua" && $Month <= 06 && $d <= 01 && $Year <= 2023) {
+      
+          }
+          
+          
+          if ($sede == "Sucursal Cagua" && $Month < 06  && $Year <= 2023) {
             $sede = 'Comerical Kagu';
             
           }
@@ -235,7 +244,7 @@ if ($_GET) {
         <tr>
 
           <td><?= $cod   ?></td>
-          <td><?= $sedes_ar[$i]  ?></td>
+          <td><?= $sede  ?></td>
 
 
           <td><?= $tot_neto_factura    ?></td>
