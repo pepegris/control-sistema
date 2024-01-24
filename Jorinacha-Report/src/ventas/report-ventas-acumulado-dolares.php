1d20cd7fc73bbe830ea08d75a8c9891d9e34b8b2
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-01-24 11:23:16 -0400

    Update report-ventas-acumulado-dolares.php

--- Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
+++ Jorinacha-Report/src/ventas/report-ventas-acumulado-dolares.php
@@ -122,18 +122,6 @@ if ($_GET) {
             $d = $e;
           }
 
-
-          $fecha =  $Year .'/'. $Month .'/'  . $d;
-          $tasas = getTasas($sede, $fecha);
-
-
-          if ($tasas != null) {
-            $tasa_v_tasas = $tasas['tasa_v'];
-          } else {
-            $tasa_v_tasas;
-          }
-
-
           if ($sede == "Sucursal Caracas I" && $Month < 04  && $Year <= 2023) {
             $sede = 'Comercial Merina';
             
@@ -158,6 +146,17 @@ if ($_GET) {
           if ($sede == "Sucursal Cagua" && $Month < 06  && $Year <= 2023) {
             $sede = 'Comercial Kagu';
             
+          }
+
+
+          $fecha =  $Year .'/'. $Month .'/'  . $d;
+          $tasas = getTasas($sede, $fecha);
+
+
+          if ($tasas != null) {
+            $tasa_v_tasas = $tasas['tasa_v'];
+          } else {
+            $tasa_v_tasas;
           }
 
 
