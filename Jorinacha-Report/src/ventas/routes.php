a4e35f6d968f898e261cbb02c59bf93d75eead0e
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-03-21 15:53:50 -0400

    up

--- Jorinacha-Report/src/ventas/routes.php
+++ Jorinacha-Report/src/ventas/routes.php
@@ -34,7 +34,13 @@ if ($clave === 'N3td0s' ) {
 
         header("refresh:1;url= report-ventas-diaria.php?fecha1=$fecha1&divisa=$divisa&sedes=$sedes&linea=$linea");
 
-    }elseif ($reporte=='dias') {
+    }elseif ($reporte=='por') {
+
+        header("refresh:1;url= report-porcentaje-mes-todo.php?fecha1=$fecha1&fecha2=$fecha2&linea=$linea&divisa=$divisa&sedes=$sedes");
+
+    }    
+    
+    elseif ($reporte=='dias') {
 
         header("refresh:1;url= report-ventas-diaria-todo.php?fecha1=$fecha1&fecha2=$fecha2&divisa=$divisa&sedes=$sedes&linea=$linea");
 
