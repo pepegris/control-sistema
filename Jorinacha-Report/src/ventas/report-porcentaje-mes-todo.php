b390c8f06ba0601e2ab65d361fef0142154318c4
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-03-22 10:28:52 -0400

    Update report-porcentaje-mes-todo.php

--- Jorinacha-Report/src/ventas/report-porcentaje-mes-todo.php
+++ Jorinacha-Report/src/ventas/report-porcentaje-mes-todo.php
@@ -1,17 +1,61 @@
-<html>
+<?php
+ini_set('memory_limit', '4096M');
+ini_set('max_execution_time', 3600);
+
+require "../../includes/log.php";
+include '../../includes/header2.php';
+include '../../services/mysql.php';
+include '../../services/adm/ventas/diarias.php';
+
+if ($_GET) {
+
+  $divisa = $_GET['divisa'];
+  $linea = $_GET['linea'];
+  $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
+  $fecha2 = date("Ymd", strtotime($_GET['fecha2']));
+
+
+  $Day = date("d", strtotime($fecha2));
+  $Month_total = date("m", strtotime($fecha2));
+  $Year = date("Y", strtotime($fecha2));
+
+
+
+
+
+?>
+  <html>
+
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
+      google.charts.load("current", {
+        packages: ["corechart"]
+      });
       google.charts.setOnLoadCallback(drawChart);
+
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+
+          <?php
+
+          $serverName = "172.16.1.39";
+          $connectionInfo = array("Database" => "$database", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+          $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+          $sql = "SELECT TOP 5 * FROM art  ";
+
+          $consulta = sqlsrv_query($conn, $sql);
+          while ($row = sqlsrv_fetch_array($consulta)) {
+
+            echo "['" .$row['art_des']. "'," .$row['co_art']. "],";
+          }
+
+
+
+
+          ?>
         ]);
 
         var options = {
@@ -24,7 +68,21 @@
       }
     </script>
   </head>
+
   <body>
     <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
   </body>
-</html>
+
+  </html>
+
+
+<?php
+
+
+} else {
+  header("location: form.php");
+}
+
+
+
+include '../../includes/footer.php'; ?>
