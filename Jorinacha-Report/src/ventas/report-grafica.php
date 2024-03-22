3780c60542ab45e31a65d23a5ab8d6917f88ec7a
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-03-22 13:34:49 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -61,16 +61,46 @@ include '../../services/adm/ventas/diarias.php';
 
               echo "['" .$row['lin_des']. " / " .$row['total_art']. "'," .$row['total_art']. "],";
             }
-
-
-
-
-
           ?>
         ]);
 
         var options = {
-          title: 'Modelos vendidos por Puerto',
+          title: 'Modelos vendidos por Puerto 2023',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Articulos', 'Total Vendidos'],
+
+          <?php
+
+            $serverName = "172.16.1.39";
+            $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+            $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+            $sql = "SELECT SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
+            inner join reng_fac on factura.fact_num=reng_fac.fact_num
+            inner join art  on art.co_art=reng_fac.co_art
+            inner join lin_art on lin_art.co_lin=art.co_lin
+            where factura.anulada =0 and factura.fec_emis between '20240101' and '20241231'
+            group by lin_art.lin_des 
+            order by total_art desc";
+
+            $consulta = sqlsrv_query($conn, $sql);
+            while ($row = sqlsrv_fetch_array($consulta)) {
+
+              echo "['" .$row['lin_des']. " / " .$row['total_art']. "'," .$row['total_art']. "],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Modelos vendidos por Puerto 2024',
           is3D: true,
         };
 
