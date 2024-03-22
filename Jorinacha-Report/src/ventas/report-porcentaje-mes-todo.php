e54abde41e56cc013a854651d6667ef09b533d86
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-03-22 11:09:36 -0400

    Update report-porcentaje-mes-todo.php

--- Jorinacha-Report/src/ventas/report-porcentaje-mes-todo.php
+++ Jorinacha-Report/src/ventas/report-porcentaje-mes-todo.php
@@ -15,22 +15,23 @@
 
           <?php
 
-          $serverName = "172.16.1.39";
-          $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
-          $conn = sqlsrv_connect($serverName, $connectionInfo);
+            $serverName = "172.16.1.39";
+            $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+            $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-          $sql = "SELECT TOP 5 SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
-          inner join reng_fac on factura.fact_num=reng_fac.fact_num
-          inner join art  on art.co_art=reng_fac.co_art
-          inner join lin_art on lin_art.co_lin=art.co_lin
-          where factura.anulada =0 and factura.fec_emis between '20230101' and '20231231'
-          group by lin_art.lin_des ";
+            $sql = "SELECT TOP 5 SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
+            inner join reng_fac on factura.fact_num=reng_fac.fact_num
+            inner join art  on art.co_art=reng_fac.co_art
+            inner join lin_art on lin_art.co_lin=art.co_lin
+            where factura.anulada =0 and factura.fec_emis between '20230101' and '20231231'
+            group by lin_art.lin_des ";
 
-          $consulta = sqlsrv_query($conn, $sql);
-          while ($row = sqlsrv_fetch_array($consulta)) {
+            $consulta = sqlsrv_query($conn, $sql);
+            while ($row = sqlsrv_fetch_array($consulta)) {
 
-            echo "['" .$row['total_art']. "'," .$row['lin_des']. "],";
-          }
+              echo "['" .$row['lin_des']. "'," .$row['total_art']. "],";
+            }
+
 
 
 
