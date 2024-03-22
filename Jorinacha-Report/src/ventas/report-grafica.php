62d92e17fe06ad6066284b9ebb91b76169e34348
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-03-22 13:46:17 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -7,15 +7,15 @@ include '../../includes/header2.php';
 include '../../services/mysql.php';
 include '../../services/adm/ventas/diarias.php';
 
-  $divisa = $_GET['divisa'];
-  $linea=$_GET['linea'];
-  $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
-  $fecha2 = date("Ymd", strtotime($_GET['fecha2']));
+$divisa = $_GET['divisa'];
+$linea = $_GET['linea'];
+$fecha1 = date("Ymd", strtotime($_GET['fecha1']));
+$fecha2 = date("Ymd", strtotime($_GET['fecha2']));
 
 
-  $Day = date("d", strtotime($fecha2));
-  $Month_total = date("m", strtotime($fecha2));
-  $Year = date("Y", strtotime($fecha2));
+$Day = date("d", strtotime($fecha2));
+$Month_total = date("m", strtotime($fecha2));
+$Year = date("Y", strtotime($fecha2));
 
 
 
@@ -24,60 +24,52 @@ include '../../services/adm/ventas/diarias.php';
 ?>
 
 <style>
-  body{
+  body {
     background-color: white;
   }
 </style>
 
 
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {
-        packages: ["corechart"]
-      });
-      google.charts.setOnLoadCallback(drawChart);
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {
+      packages: ["corechart"]
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Articulos', 'Total Vendidos'],
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Articulos', 'Total Vendidos'],
 
-          <?php
+        <?php
 
-            $serverName = "172.16.1.39";
-            $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
-            $conn = sqlsrv_connect($serverName, $connectionInfo);
+        for ($i = 1; $i < count($sedes_ar); $i++) {
 
-            $sql = "SELECT SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
-            inner join reng_fac on factura.fact_num=reng_fac.fact_num
-            inner join art  on art.co_art=reng_fac.co_art
-            inner join lin_art on lin_art.co_lin=art.co_lin
-            where factura.anulada =0 and factura.fec_emis between '20230101' and '20231231'
-            group by lin_art.lin_des 
-            order by total_art desc";
+          $sede = $sedes_ar[$i];
+          $vendidos = getTasas($sede, $fecha1,$fecha2);
+        }
 
-            $consulta = sqlsrv_query($conn, $sql);
-            while ($row = sqlsrv_fetch_array($consulta)) {
 
-              echo "['" .$row['lin_des']. " / " .$row['total_art']. "'," .$row['total_art']. "],";
-            }
-          ?>
-        ]);
+        while ($row = sqlsrv_fetch_array($consulta)) {
 
-        var options = {
-          title: 'Modelos vendidos por Puerto',
-          is3D: true,
-        };
+          echo "['" . $row['lin_des'] . " / " . $row['total_art'] . "'," . $row['total_art'] . "],";
+        }
+        ?>
+      ]);
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
+      var options = {
+        title: 'Modelos vendidos por Puerto',
+        is3D: true,
+      };
 
-      
-    </script>
-  </head>
+      var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+      chart.draw(data, options);
+    }
+  </script>
+</head>
 
-    <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+<div id="piechart_3d" style="width: 900px; height: 500px;"></div>
 
 
 
