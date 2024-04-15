d54acea20863db893977ea4b015b8c143640e301
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-15 10:58:24 -0400

    u

---
+++ Jorinacha-Report/src/ventas/report-grafica copy 2.php
@@ -0,0 +1,87 @@
+<?php
+ini_set('memory_limit', '4096M');
+ini_set('max_execution_time', 3600);
+
+#require "../../includes/log.php";
+include '../../includes/header2.php';
+include '../../services/mysql.php';
+include '../../services/adm/ventas/diarias.php';
+
+$divisa = $_GET['divisa'];
+$linea = $_GET['linea'];
+$fecha1 = date("Ymd", strtotime($_GET['fecha1']));
+$fecha2 = date("Ymd", strtotime($_GET['fecha2']));
+
+
+$Day = date("d", strtotime($fecha2));
+$Month_total = date("m", strtotime($fecha2));
+$Year = date("Y", strtotime($fecha2));
+
+
+
+
+
+?>
+
+<style>
+  body {
+    background-color: white;
+  }
+</style>
+
+
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {
+      packages: ["corechart"]
+    });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Articulos', 'Total Vendidos'],
+
+        <?php
+
+        for ($i = 1; $i < count($sedes_ar); $i++) {
+
+          $sede = $sedes_ar[$i];
+          $vendidos = getVendido_Grafica($sede, $fecha1,$fecha2);
+          var_dump(count($vendidos));
+          echo "<br>";
+          var_dump($vendidos);
+
+/* 
+          for ($e = 1; $e < count($vendidos); $e++) {
+
+            $vendidos = insertVendido_Grafica();
+            
+          } */
+
+        }
+
+
+        while ($row = sqlsrv_fetch_array($consulta)) {
+
+          echo "['" . $row['lin_des'] . " / " . $row['total_art'] . "'," . $row['total_art'] . "],";
+        }
+        ?>
+      ]);
+
+      var options = {
+        title: 'Modelos vendidos por Puerto',
+        is3D: true,
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+      chart.draw(data, options);
+    }
+  </script>
+</head>
+
+<div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+
+
+
+<?php include '../../includes/footer.php'; ?>
