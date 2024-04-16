4db63d4f73c395817979e945579950cd768ae0b1
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-16 14:21:10 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -32,11 +32,58 @@ include '../../services/adm/ventas/diarias.php';
 
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
     <script type="text/javascript">
+
       google.charts.load("current", {
         packages: ["corechart"]
       });
       google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Articulos', 'Total Vendidos'],
+
+          <?php
+
+            $serverName = "172.16.1.39";
+            $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+            $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+            $sql = "SELECT SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
+            inner join reng_fac on factura.fact_num=reng_fac.fact_num
+            inner join art  on art.co_art=reng_fac.co_art
+            inner join lin_art on lin_art.co_lin=art.co_lin
+            where factura.anulada =0 and factura.fec_emis between '20230101' and '20231231'
+            group by lin_art.lin_des 
+            order by total_art desc";
+
+            $consulta = sqlsrv_query($conn, $sql);
+            while ($row = sqlsrv_fetch_array($consulta)) {
+
+              echo "['" .$row['lin_des']. " / " .$row['total_art']. "'," .$row['total_art']. "],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Modelos vendidos por Puerto',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+
+
+
+
+
+
+
+
+
+
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
