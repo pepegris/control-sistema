6cdb8c297f4478544f26fd02c0c5543d4bec0b43
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 09:16:59 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -123,7 +123,7 @@ include '../../services/adm/ventas/diarias.php';
 
 
 
-      var data = google.visualization.arrayToDataTable([
+/*       var data = google.visualization.arrayToDataTable([
          ['Element', 'Density', { role: 'style' }],
          ['Copper', 8.94, '#b87333'],            // RGB value
          ['Silver', 10.49, 'silver'],            // English color name
@@ -131,7 +131,7 @@ include '../../services/adm/ventas/diarias.php';
 
        ['Platinum', 21.45, 'color: #e5e4e2' ], // CSS-style declaration
       ]);
-      
+       */
     </script>
     
   </head>
