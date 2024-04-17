02de777d6ddfb45a155418bf904abbb4632cb4c2
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 10:02:02 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -119,31 +119,8 @@ $Year = date("Y", strtotime($fecha2));
       chart.draw(data, options);
     }
 
-    function drawSaarahChart() {
-
-      // Create the data table for Sarah's pizza.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Topping');
-      data.addColumn('number', 'Slices');
-      data.addRows([
-        ['Mushrooms', 1],
-        ['Onions', 1],
-        ['Olives', 2],
-        ['Zucchini', 2],
-        ['Pepperoni', 1]
-      ]);
-
-      // Set options for Sarah's pie chart.
-      var options = {
-        title: 'How Much Pizza Sarah Ate Last Night',
-        width: 400,
-        height: 300
-      };
-
-      // Instantiate and draw the chart for Sarah's pizza.
-      var chart = new google.visualization.PieChart(document.getElementById('Saarah_chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('Saarah_chart_div'));
       chart.draw(data, options);
-    }
 
     // Callback that draws the pie chart for Anthony's pizza.
     function drawAnthonyChart() {
