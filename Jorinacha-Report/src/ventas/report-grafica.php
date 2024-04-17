8467e1ff36b59f2ee38c465f48d7f35d848ef0ab
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 08:29:05 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -72,7 +72,7 @@ include '../../services/adm/ventas/diarias.php';
           is3D: true,
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('barchart_div'));
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
         chart.draw(data, options);
       }
 
@@ -116,6 +116,21 @@ include '../../services/adm/ventas/diarias.php';
 
 
 
+
+
+
+
+
+
+
+      var data = google.visualization.arrayToDataTable([
+         ['Element', 'Density', { role: 'style' }],
+         ['Copper', 8.94, '#b87333'],            // RGB value
+         ['Silver', 10.49, 'silver'],            // English color name
+         ['Gold', 19.30, 'gold'],
+
+       ['Platinum', 21.45, 'color: #e5e4e2' ], // CSS-style declaration
+      ]);
       
     </script>
     
