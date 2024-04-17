410f2001bd6eb79f5fc7092f34a2351c2c403f84
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 09:46:08 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -7,15 +7,15 @@ include '../../includes/header2.php';
 include '../../services/mysql.php';
 include '../../services/adm/ventas/diarias.php';
 
-  $divisa = $_GET['divisa'];
-  $linea=$_GET['linea'];
-  $fecha1 = date("Ymd", strtotime($_GET['fecha1']));
-  $fecha2 = date("Ymd", strtotime($_GET['fecha2']));
+$divisa = $_GET['divisa'];
+$linea = $_GET['linea'];
+$fecha1 = date("Ymd", strtotime($_GET['fecha1']));
+$fecha2 = date("Ymd", strtotime($_GET['fecha2']));
 
 
-  $Day = date("d", strtotime($fecha2));
-  $Month_total = date("m", strtotime($fecha2));
-  $Year = date("Y", strtotime($fecha2));
+$Day = date("d", strtotime($fecha2));
+$Month_total = date("m", strtotime($fecha2));
+$Year = date("Y", strtotime($fecha2));
 
 
 
@@ -24,31 +24,31 @@ include '../../services/adm/ventas/diarias.php';
 ?>
 
 <style>
-  body{
+  body {
     background-color: white;
   }
 </style>
 
 
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {
-        packages: ["corechart"]
-      });
-      google.charts.setOnLoadCallback(drawChart);
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {
+      packages: ["corechart"]
+    });
+    google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Articulos', 'Total Vendidos'],
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Articulos', 'Total Vendidos'],
 
-          <?php
+        <?php
 
-            $serverName = "172.16.1.39";
-            $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
-            $conn = sqlsrv_connect($serverName, $connectionInfo);
+        $serverName = "172.16.1.39";
+        $connectionInfo = array("Database" => "PUECRUZ", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-            $sql = "SELECT SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
+        $sql = "SELECT SUM (CONVERT(numeric(10,0), reng_fac.total_art)) as total_art,lin_art.lin_des from factura 
             inner join reng_fac on factura.fact_num=reng_fac.fact_num
             inner join art  on art.co_art=reng_fac.co_art
             inner join lin_art on lin_art.co_lin=art.co_lin
@@ -56,29 +56,107 @@ include '../../services/adm/ventas/diarias.php';
             group by lin_art.lin_des 
             order by total_art desc";
 
-            $consulta = sqlsrv_query($conn, $sql);
-            while ($row = sqlsrv_fetch_array($consulta)) {
+        $consulta = sqlsrv_query($conn, $sql);
+        while ($row = sqlsrv_fetch_array($consulta)) {
 
-              echo "['" .$row['lin_des']. " / " .$row['total_art']. "'," .$row['total_art']. "],";
-            }
-          ?>
-        ]);
+          echo "['" . $row['lin_des'] . " / " . $row['total_art'] . "'," . $row['total_art'] . "],";
+        }
+        ?>
+      ]);
 
-        var options = {
-          title: 'Modelos vendidos por Puerto',
-          is3D: true,
-        };
+      var options = {
+        title: 'Modelos vendidos por Puerto',
+        is3D: true,
+      };
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
+      var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+      chart.draw(data, options);
+    }
 
-      
-    </script>
-  </head>
 
-    <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
 
+
+
+
+
+    //---------------------------------------------------------------------------------------------||
+
+    // Load Charts and the corechart package.
+    google.charts.load('current', {
+      'packages': ['corechart']
+    });
+
+    // Draw the pie chart for Sarah's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawSarahChart);
+
+    // Draw the pie chart for the Anthony's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawAnthonyChart);
+
+    // Callback that draws the pie chart for Sarah's pizza.
+    function drawSarahChart() {
+
+      // Create the data table for Sarah's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 1],
+        ['Onions', 1],
+        ['Olives', 2],
+        ['Zucchini', 2],
+        ['Pepperoni', 1]
+      ]);
+
+      // Set options for Sarah's pie chart.
+      var options = {
+        title: 'How Much Pizza Sarah Ate Last Night',
+        width: 400,
+        height: 300
+      };
+
+      // Instantiate and draw the chart for Sarah's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('Sarah_chart_div'));
+      chart.draw(data, options);
+    }
+
+    // Callback that draws the pie chart for Anthony's pizza.
+    function drawAnthonyChart() {
+
+      // Create the data table for Anthony's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 2],
+        ['Onions', 2],
+        ['Olives', 2],
+        ['Zucchini', 0],
+        ['Pepperoni', 3]
+      ]);
+
+      // Set options for Anthony's pie chart.
+      var options = {
+        title: 'How Much Pizza Anthony Ate Last Night',
+        width: 400,
+        height: 300
+      };
+
+      // Instantiate and draw the chart for Anthony's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('Anthony_chart_div'));
+      chart.draw(data, options);
+    }
+  </script>
+</head>
+
+<div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+
+    <!--Table and divs that hold the pie charts-->
+    <table class="columns">
+      <tr>
+        <td><div id="Sarah_chart_div" style="border: 1px solid #ccc"></div></td>
+        <td><div id="Anthony_chart_div" style="border: 1px solid #ccc"></div></td>
+      </tr>
+    </table>
 
 
 <?php include '../../includes/footer.php'; ?>
