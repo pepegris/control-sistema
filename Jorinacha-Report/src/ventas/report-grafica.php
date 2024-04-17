06451587520915774f60f9c37ba2e8f86654bed3
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 09:41:58 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -32,10 +32,7 @@ include '../../services/adm/ventas/diarias.php';
 
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
     <script type="text/javascript">
-
-      // GRAFICAS GENERAL
       google.charts.load("current", {
         packages: ["corechart"]
       });
@@ -76,94 +73,11 @@ include '../../services/adm/ventas/diarias.php';
         chart.draw(data, options);
       }
 
-
-
-      // VARIAS GRAFICAS
-
-      // Load Charts and the corechart and barchart packages.
-      google.charts.load('current', {'packages':['corechart']});
-
-      // Draw the pie chart and bar chart when Charts is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        var piechart_options = {title:'Pie Chart: How Much Pizza I Ate Last Night',
-                       width:400,
-                       height:300};
-        var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
-        piechart.draw(data, piechart_options);
-
-        var barchart_options = {title:'Barchart: How Much Pizza I Ate Last Night',
-                       width:400,
-                       height:300,
-                       legend: 'none'};
-        var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
-        barchart.draw(data, barchart_options);
-      }
-
-
-
-
-
-
-
-
-
-
-
-/*       var data = google.visualization.arrayToDataTable([
-         ['Element', 'Density', { role: 'style' }],
-         ['Copper', 8.94, '#b87333'],            // RGB value
-         ['Silver', 10.49, 'silver'],            // English color name
-         ['Gold', 19.30, 'gold'],
-
-       ['Platinum', 21.45, 'color: #e5e4e2' ], // CSS-style declaration
-      ]);
-       */
+      
     </script>
-    
   </head>
 
-
-
-
-
-
-
-
     <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
-
-
-
-    <!--Table and divs that hold the pie charts-->
-    <table class="columns">
-      <tr>
-        <td><div id="piechart_div" style="border: 1px solid #ccc"></div></td>
-        <td><div id="barchart_div" style="border: 1px solid #ccc"></div></td>
-      </tr>
-    </table>
-
-
-
-
-
-
-
-
-
-    
 
 
 
