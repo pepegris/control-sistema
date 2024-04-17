b88667951ca06282a0f21a0508ddf131db644fe6
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-04-17 10:33:45 -0400

    Update report-grafica.php

--- Jorinacha-Report/src/ventas/report-grafica.php
+++ Jorinacha-Report/src/ventas/report-grafica.php
@@ -22,7 +22,7 @@ for ($i = 1; $i < count($sedes_ar); $i++) {
   $sede = $sedes_ar[$i];
   $ventas=getVendido_Grafica($sede,'20230101','20231231');
   var_dump($ventas);
-  var_dump($ventas[['lin_des']]);
+  var_dump($ventas['lin_des']);
   echo "<br>";
 
   while ($row = sqlsrv_fetch_array($ventas)) {
