5f16f040ed297ae10d15c3a6e4ae9bcd26f1c783
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-08-28 14:42:36 -0400

    Update inv.php

--- Jorinacha-Report/services/adm/inv/inv.php
+++ Jorinacha-Report/services/adm/inv/inv.php
@@ -267,7 +267,7 @@ function getreng_stock_teorico($marca,$database,$fecha1,$alma)
             ON F.num_fis=RF.num_fis
             INNER JOIN art 
             ON art.co_art=RF.co_art
-            WHERE RF.co_alma='$alma'  and art.co_lin='$marca' and f.fecha_fis='$fecha1' ";
+            WHERE RF.co_alma='$alma'  and art.co_lin='$marca' and f.fecha_fis>='$fecha1' ";
 
             $consulta = sqlsrv_query($conn, $sql);
 
