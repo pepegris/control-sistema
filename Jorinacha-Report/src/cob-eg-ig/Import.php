f01539037ddc39abdd77cbee92e9a1c97e9335f2
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-10-16 13:25:55 -0400

    Update Import.php

--- Jorinacha-Report/src/cob-eg-ig/Import.php
+++ Jorinacha-Report/src/cob-eg-ig/Import.php
@@ -36,11 +36,16 @@ if (isset($_POST)) {
     } */
     echo "<center><h3 style='color:white'>$mensaje</h3></center>";
     echo "<center><a href='Import-database.php' class='btn btn-success'>Volver</a></center>";
-?>
 
+
+} else {
+    header('refresh:1;url= Import-database.php');
+    exit;
+}
+?>
 <div id="hasta5Min" style="color: red; font-weight: bolder;"></div>
 <div id="despues5Min" style="color: green; font-weight: bolder;"></div>
-<script type="module" src="">
+<script>
     hasta5Min = document.getElementById("hasta5Min");
         despues5Min = document.getElementById("despues5Min");
         // Declaro variables para después de los 5 minutos.
@@ -69,10 +74,5 @@ if (isset($_POST)) {
             }
         },1000);
 </script>
-
-<?php
-
-} else {
-    header('refresh:1;url= Import-database.php');
-    exit;
-}
+<div id="hasta5Min" style="color: red; font-weight: bolder;"></div>
+<div id="despues5Min" style="color: green; font-weight: bolder;"></div>
