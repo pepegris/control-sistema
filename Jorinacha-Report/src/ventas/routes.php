15ecc029c394cdc523f9495aa3d278bf3ed3aae4
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2024-11-20 15:39:14 -0400

    Update routes.php

--- Jorinacha-Report/src/ventas/routes.php
+++ Jorinacha-Report/src/ventas/routes.php
@@ -42,6 +42,10 @@ if ($clave === 'N3td0s' ) {
 
         header("refresh:1;url= report-despacho-gf.php?fecha1=$fecha1&fecha2=$fecha2&linea=$linea&divisa=$divisa&sedes=$sedes");
 
+    }  elseif ($reporte=='des') {
+
+        header("refresh:1;url= report-cashea.php?fecha1=$fecha1&fecha2=$fecha2&divisa=$divisa&sedes=$sedes");
+
     }    
     
     elseif ($reporte=='dias') {
