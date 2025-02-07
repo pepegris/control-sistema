80667ddd4ab5118839c8192b93f938703acd2c9e
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2025-02-07 10:12:05 -0400

    Update factura.php

--- Jorinacha-Report/src/Fac Jorinacha/factura.php
+++ Jorinacha-Report/src/Fac Jorinacha/factura.php
@@ -1,176 +1,213 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
-    <link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="bootstrap.css">
 	<title>Factura</title>
 
-	<style>@import url(http://fonts.googleapis.com/css?family=Bree+Serif);
-  			body, h1, h2, h3, h4, h5, h6{
-    			font-family: 'Bree Serif', serif;
- 	 									}
-	@media (max-width: 900px){
-    #panel input{
-        width: 105px;
-        height: 30px;
-    }
-	#factura input{
-        width: 150px;
-        height: 30px;
-    }
-	
-    
-}				  
-			
-  	</style>
+	<style>
+		@import url(http://fonts.googleapis.com/css?family=Bree+Serif);
+
+		body,
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			font-family: 'Bree Serif', serif;
+		}
+
+		@media (max-width: 900px) {
+			#panel input {
+				width: 105px;
+				height: 30px;
+			}
+
+			#factura input {
+				width: 150px;
+				height: 30px;
+			}
 
 
-<?php 
+		}
+
+		.flex-container {
+			display: flex;
+			justify-content: space-evenly;
+			background-color: DodgerBlue;
+		}
+
+		.flex-container>div {
+			background-color: #f1f1f1;
+			width: 100px;
+			margin: 10px;
+			text-align: center;
+			line-height: 75px;
+			font-size: 30px;
+		}
+	</style>
 
 
- 
- if ($_POST) {
-    $empresa = $_POST['empresa'];
-    $factura = $_POST['factura'];
+	<?php
 
-}
 
-?>
-<br>
-<br>
-<br><br><br><br><br>
-		<div class="container">
-			<div class="row">
-            
 
-		<div class="col-xs-6 text-right">
-							<div class="panel panel-default">
-							<div class="panel-heading">
-                                    <h4>empresa
-									</h4>
-									<h4>RIF: 
-										rif
-									</h4>
-									
-							</div>
-							<div class="panel-body" id="factura">
-								<h4>Orden Nº : 
-									NUMERO
-								</h4>
-							</div>
-						</div>
+	if ($_POST) {
+		$empresa = $_POST['empresa'];
+		$factura = $_POST['factura'];
+	}
+
+	?>
+	<br><br><br><br><br>
+	<br><br><br><br><br>
+	<br><br><br><br><br>
+	<div class="container">
+		<div class="row">
+
+			<div class="flex-container">
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+			</div>
+
+
+
+
+			<div class="col-xs-6 text-right">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>CLIENTE:
+						</h4>
+						<h4>TELEFONO:
+						</h4>
+						<h4>RIF:
+						</h4>
+
 					</div>
- 
-			<hr />
- 
-			
-				<h1 style="text-align: center;">Nota de Entrega</h1> 
-			
-				<div class="row">
-					<div class="col-xs-12">
-						<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4> <?php echo "Caracas " . date("d") . " del " . date("m") . " de " . date("Y"); ?> 
-									
-									</h4>
-								</div>
-						<div class="panel-body" id="panel" >
-						
-							
-								<h4>Destino:  DIR / 
-                                Equipo: Descrip
-								</h4>
-					
-						</div>
-						</div>
+					<div class="panel-body" id="factura">
+						<h4>Orden Nº :
+							NUMERO
+						</h4>
 					</div>
-					
 				</div>
-<pre></pre>
-<table class="table table-bordered">
-	<thead >
-		<tr >
-			<th style="text-align: center;">
-				<h4>DESCRIPCION</h4>
-			</th>
-			<th style="text-align: center;">
-				<h4>CANT</h4>
-			</th>
-			<th style="text-align: center;">
-				<h4>PRECIO</h4>
-			</th>
-			<th style="text-align: center;">
-				<h4>IMPORTE</h4>
-			</th>
-			
-		</tr>
-	</thead>
-	<tbody>
-	
-		<tr>
-			<td style="text-align: center;">EQUIOP</td>
+			</div>
 
-			<td style="text-align: center;">ESTA</td>
-			
-			<td class=" text-align: center; ">A></td>
-			<td class=" text-align: center;">B></td>
-			
-			
-		</tr>
-
-			
-		<tr>
-			<td style="text-align: center;">EQUIOP</td>
-
-			<td style="text-align: center;">ESTA</td>
-			
-			<td class=" text-align: center; ">A></td>
-			<td class=" text-align: center;">B></td>
-			
-			
-		</tr>
-
-        <tr>
-			<td>&nbsp;</td>
-			<td><a href="#"> &nbsp; </a></td>
-			<td class="text-right">&nbsp;</td>
-			<td class="text-right ">&nbsp;</td>
-			
-		</tr> 
-
-		<tr >
-        
-			<td colspan="3" style="text-align: right;">Recibido Firma :</td>
-			<td style="text-align: right;"><hr></td>
-			
-			
-		</tr> 
-		
-		
-	</tbody>
-</table>
-
-		
+			<hr />
 
 
-		
-</div>
+			<h1 style="text-align: center;">Nota de Entrega</h1>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4> <?php echo "Caracas " . date("d") . " del " . date("m") . " de " . date("Y"); ?>
+
+							</h4>
+						</div>
+						<div class="panel-body" id="panel">
+
+
+							<h4>Destino: DIR /
+								Equipo: Descrip
+							</h4>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<pre></pre>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th style="text-align: center;">
+							<h4>DESCRIPCION</h4>
+						</th>
+						<th style="text-align: center;">
+							<h4>CANT</h4>
+						</th>
+						<th style="text-align: center;">
+							<h4>PRECIO</h4>
+						</th>
+						<th style="text-align: center;">
+							<h4>IMPORTE</h4>
+						</th>
+
+					</tr>
+				</thead>
+				<tbody>
+
+					<tr>
+						<td style="text-align: center;">EQUIOP</td>
+
+						<td style="text-align: center;">ESTA</td>
+
+						<td class=" text-align: center; ">A></td>
+						<td class=" text-align: center;">B></td>
+
+
+					</tr>
+
+
+					<tr>
+						<td style="text-align: center;">EQUIOP</td>
+
+						<td style="text-align: center;">ESTA</td>
+
+						<td class=" text-align: center; ">A></td>
+						<td class=" text-align: center;">B></td>
+
+
+					</tr>
+
+					<tr>
+						<td>&nbsp;</td>
+						<td><a href="#"> &nbsp; </a></td>
+						<td class="text-right">&nbsp;</td>
+						<td class="text-right ">&nbsp;</td>
+
+					</tr>
+
+					<tr>
+
+						<td colspan="3" style="text-align: right;">Recibido Firma :</td>
+						<td style="text-align: right;">
+							<hr>
+						</td>
+
+
+					</tr>
+
+
+				</tbody>
+			</table>
 
 
 
-    <p class="text-right ">der</p>
-    <p class="text-right "><b>Dir: </b>der</p>
-    <p class="text-right "><b>Tlf: </b>der/p>
+
+
+		</div>
 
 
 
-</div>
+		<p class="text-right ">der</p>
+		<p class="text-right "><b>Dir: </b>der</p>
+		<p class="text-right "><b>Tlf: </b>der/p>
+
+
+
+	</div>
 
 
 </head>
+
 <body>
 	<center><input type="button" name="imprimir" value="Imprimir" onclick="window.print();"> </center>
 	<br>
-	
+
 </body>
+
 </html>
