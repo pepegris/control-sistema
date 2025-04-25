58c4aa5d0f09dd2f559aa5316db101021a2b489d
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2025-04-25 12:43:58 -0400

    Update cashea.php

--- Jorinacha-Report/services/adm/ventas/cashea.php
+++ Jorinacha-Report/services/adm/ventas/cashea.php
@@ -35,7 +35,8 @@ function getCobros_Cashea($sede, $fecha1, $fecha2)
 
             while ($row = sqlsrv_fetch_array($consulta)) {
 
-/*                 $cobros['cob_num'] = $row['cob_num'];
+/*                 
+                $cobros['cob_num'] = $row['cob_num'];
                 $cobros['fec_cob'] = $row['fec_cob'];
                 $cobros['doc_num'] = $row['doc_num'];
                 $cobros['mont_doc'] = $row['mont_doc'];
