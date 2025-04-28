f8f7fd3934997348d530b1698389c2993fb9c0ba
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2025-04-28 12:58:46 -0400

    Update cashea.php

--- Jorinacha-Report/services/adm/ventas/cashea.php
+++ Jorinacha-Report/services/adm/ventas/cashea.php
@@ -19,7 +19,7 @@ function getCobros_Cashea($sede, $fecha1, $fecha2)
             $serverName = "172.16.1.39";
             $connectionInfo = array("Database" => "$database", "UID" => "mezcla", "PWD" => "Zeus33$", "CharacterSet" => "UTF-8");
             $conn = sqlsrv_connect($serverName, $connectionInfo);
-/* 
+            /* 
             $sql = "SELECT cobros.cob_num,cobros.fec_cob,reng_cob.doc_num  ,CONVERT(numeric(10,2), reng_tip.mont_doc) as mont_doc, 
                     factura.tot_neto ,clientes.co_cli,clientes.cli_des , sum(CONVERT(numeric(10,0), reng_fac.total_art)) as total_art
                     from cobros
@@ -30,33 +30,47 @@ function getCobros_Cashea($sede, $fecha1, $fecha2)
                     where banco='CASHEA'  and cobros.fec_cob between '$fecha1' and '$fecha2' and cobros.anulado=0
                     group by cobros.cob_num,cobros.fec_cob,reng_cob.doc_num ,reng_tip.mont_doc,factura.tot_neto,clientes.co_cli,clientes.cli_des,reng_fac.total_art"; */
 
- 
+
             $sql = "SELECT cobros.cob_num,cobros.fec_cob,reng_cob.doc_num  ,CONVERT(numeric(10,2), reng_tip.mont_doc) as mont_doc, 
                     factura.tot_neto ,clientes.co_cli,clientes.cli_des from cobros
                     join reng_tip on cobros.cob_num=reng_tip.cob_num
                     join reng_cob on cobros.cob_num=reng_cob.cob_num
 					join factura on reng_cob.doc_num=factura.fact_num
 					join clientes on clientes.co_cli=factura.co_cli
-                    where  cod_caja='CASHEA' and  cobros.fec_cob between '$fecha1' and '$fecha2' and cobros.anulado=0"; 
+                    where  cod_caja='CASHEA' and  cobros.fec_cob between '$fecha1' and '$fecha2' and cobros.anulado=0";
+
 
 
 
             $consulta = sqlsrv_query($conn, $sql);
 
+            if ($consulta != null) {
 
-            while ($row = sqlsrv_fetch_array($consulta)) {
+                while ($row = sqlsrv_fetch_array($consulta)) {
 
-/*                 
-                $cobros['cob_num'] = $row['cob_num'];
-                $cobros['fec_cob'] = $row['fec_cob'];
-                $cobros['doc_num'] = $row['doc_num'];
-                $cobros['mont_doc'] = $row['mont_doc'];
-                $cobros['tot_neto'] = $row['tot_neto'];
-                $cobros['co_cli'] = $row['co_cli'];
-                $cobros['cli_des'] = $row['cli_des'];
- */
-                $cobros[] = $row;
+                    $cobros[] = $row;
+                }
+            } else {
+
+
+                $sql = "SELECT cobros.cob_num,cobros.fec_cob,reng_cob.doc_num  ,CONVERT(numeric(10,2), reng_tip.mont_doc) as mont_doc, 
+                factura.tot_neto ,clientes.co_cli,clientes.cli_des from cobros
+                join reng_tip on cobros.cob_num=reng_tip.cob_num
+                join reng_cob on cobros.cob_num=reng_cob.cob_num
+                join factura on reng_cob.doc_num=factura.fact_num
+                join clientes on clientes.co_cli=factura.co_cli
+                where  banco='CASHEA' and  cobros.fec_cob between '$fecha1' and '$fecha2' and cobros.anulado=0";
+
+                $consulta = sqlsrv_query($conn, $sql);
+
+                while ($row = sqlsrv_fetch_array($consulta)) {
+
+                    $cobros[] = $row;
+                }
             }
+
+
+
 
             $res = $cobros;
 
@@ -139,10 +153,10 @@ function getDeposito_Cashea($sede, $cob_num)
             if ($consulta != null) {
                 while ($row = sqlsrv_fetch_array($consulta)) {
 
-                    
-                $dep_caj['dep_num'] = $row['dep_num'];
-                $dep_caj['descrip'] = $row['descrip'];
-                break;
+
+                    $dep_caj['dep_num'] = $row['dep_num'];
+                    $dep_caj['descrip'] = $row['descrip'];
+                    break;
                 }
 
                 $res = $dep_caj;
