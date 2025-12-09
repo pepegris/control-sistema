d71edf64099adb7f376cbd04ff897c977ed2db1f
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2025-12-09 11:50:43 -0400

    Update config_replicas.php

--- Jorinacha-Report/services/adm/replica/config_replicas.php
+++ Jorinacha-Report/services/adm/replica/config_replicas.php
@@ -2,7 +2,7 @@
 // config_replicas.php
 // LISTA MAESTRA VALIDADA (IP VPN + INSTANCIA)
 
-/* $lista_replicas = [
+ $lista_replicas = [
     'ACARIGUA'     => ['ip' => '26.35.57.24',           'db' => 'ACARIGUA', 'publicacion' => 'ACARIGUA'],
     'APURE'        => ['ip' => '26.77.57.145',          'db' => 'APURA',    'publicacion' => 'APURA'],
     'CAGUA'        => ['ip' => '26.179.47.19\KAGUPREV', 'db' => 'CAGUA',    'publicacion' => 'CAGUA'],
@@ -19,28 +19,8 @@
     'PUERTO'       => ['ip' => '26.80.3.26',            'db' => 'PUERTOX',  'publicacion' => 'PUERTO'],
     'VALENCIA'     => ['ip' => '26.42.16.172\VALENA',   'db' => 'VALE_A21', 'publicacion' => 'VALENA'],
     'VALLE PASCUA' => ['ip' => '26.214.82.1\VALLE',     'db' => 'VAPASCUA', 'publicacion' => 'VAPASCUA']
-]; */
+]; 
 
 
-$lista_replicas = [
-    // --- ESTAS 2 DEBEN FUNCIONAR (Datos Reales) ---
-    'ACARIGUA'     => ['ip' => '26.35.57.24',           'db' => 'ACARIGUA', 'publicacion' => 'ACARIGUA'],
-    'APURE'        => ['ip' => '26.77.57.145',          'db' => 'APURA',    'publicacion' => 'APURA'],
 
-    // --- ESTAS DARÁN ERROR (IPs Falsas para prueba) ---
-    'CAGUA'        => ['ip' => '0.0.0.0', 'db' => 'CAGUA',    'publicacion' => 'CAGUA'],
-    'CARACAS 1'    => ['ip' => '0.0.0.0', 'db' => 'CARACAS1', 'publicacion' => 'CARACAS1'],
-    'CORO 1'       => ['ip' => '0.0.0.0', 'db' => 'CORO1',    'publicacion' => 'CORO1'],
-    'CORO 2'       => ['ip' => '0.0.0.0', 'db' => 'CORO2',    'publicacion' => 'CORO2'],
-    'CORO 3'       => ['ip' => '0.0.0.0', 'db' => 'CORO3',    'publicacion' => 'CORO3'],
-    'GUIGUE'       => ['ip' => '0.0.0.0', 'db' => 'GUIGE_A',  'publicacion' => 'GUIGUE'],
-    'HIGUEROTE'    => ['ip' => '0.0.0.0', 'db' => 'HIGUE',    'publicacion' => 'HIGUE'],
-    'MATURIN'      => ['ip' => '0.0.0.0', 'db' => 'MATURIN',  'publicacion' => 'MATURIN'],
-    'OJEDA'        => ['ip' => '0.0.0.0', 'db' => 'OJEDA',    'publicacion' => 'OJEDA'],
-    'PUNTO FIJO 1' => ['ip' => '0.0.0.0', 'db' => 'PTOFIJO1', 'publicacion' => 'PTOFIJO1'],
-    'PUNTO FIJO 2' => ['ip' => '0.0.0.0', 'db' => 'PTOFIJO2', 'publicacion' => 'PTOFIJO2'],
-    'PUERTO'       => ['ip' => '0.0.0.0', 'db' => 'PUERTOX',  'publicacion' => 'PUERTO'],
-    'VALENCIA'     => ['ip' => '0.0.0.0', 'db' => 'VALE_A21', 'publicacion' => 'VALENA'],
-    'VALLE PASCUA' => ['ip' => '0.0.0.0', 'db' => 'VAPASCUA', 'publicacion' => 'VAPASCUA']
-];
 ?>
