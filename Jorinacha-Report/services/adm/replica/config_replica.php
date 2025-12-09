0526beee4217aa40d0725999e7ed0ba61f48bb98
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2025-12-09 11:09:37 -0400

    Update config_replica.php

--- Jorinacha-Report/services/adm/replica/config_replica.php
+++ Jorinacha-Report/services/adm/replica/config_replica.php
@@ -2,22 +2,45 @@
 // config_replicas.php
 // LISTA MAESTRA VALIDADA (IP VPN + INSTANCIA)
 
-$lista_replicas = [
-    'ACARIGUA'     => ['ip' => '12222.35.57.24',           'db' => 'ACARIGUA', 'publicacion' => 'ACARIGUA'],
-    'APURE'        => ['ip' => '12222.77.57.145',          'db' => 'APURA',    'publicacion' => 'APURA'],
-    'CAGUA'        => ['ip' => '12222.179.47.19\KAGUPREV', 'db' => 'CAGUA',    'publicacion' => 'CAGUA'],
-    'CARACAS 1'    => ['ip' => '12222.163.49.171',         'db' => 'CARACAS1', 'publicacion' => 'CARACAS1'],
-    'CORO 1'       => ['ip' => '12222.40.138.208',         'db' => 'CORO1',    'publicacion' => 'CORO1'],
-    'CORO 2'       => ['ip' => '12222.10.12.209',          'db' => 'CORO2',    'publicacion' => 'CORO2'],
-    'CORO 3'       => ['ip' => '12222.20.32.25\CORINA2',   'db' => 'CORO3',    'publicacion' => 'CORO3'],
-    'GUIGUE'       => ['ip' => '12222.160.180.159\CATICA2','db' => 'GUIGE_A',  'publicacion' => 'GUIGUE'],
-    'HIGUEROTE'    => ['ip' => '12222.185.48.247',         'db' => 'HIGUE',    'publicacion' => 'HIGUE'],
-    'MATURIN'      => ['ip' => '12222.25.243.193',         'db' => 'MATURIN',  'publicacion' => 'MATURIN'],
-    'OJEDA'        => ['ip' => '12222.173.204.113\OJENA',  'db' => 'OJEDA',    'publicacion' => 'OJEDA'],
-    'PUNTO FIJO 1' => ['ip' => '12222.248.191.230\PUFIJO', 'db' => 'PTOFIJO1', 'publicacion' => 'PTOFIJO1'],
-    'PUNTO FIJO 2' => ['ip' => '12222.229.9.68\NACHARI',   'db' => 'PTOFIJO2', 'publicacion' => 'PTOFIJO2'],
-    'PUERTO'       => ['ip' => '12222.80.3.26',            'db' => 'PUERTOX',  'publicacion' => 'PUERTO'],
+/* $lista_replicas = [
+    'ACARIGUA'     => ['ip' => '26.35.57.24',           'db' => 'ACARIGUA', 'publicacion' => 'ACARIGUA'],
+    'APURE'        => ['ip' => '26.77.57.145',          'db' => 'APURA',    'publicacion' => 'APURA'],
+    'CAGUA'        => ['ip' => '26.179.47.19\KAGUPREV', 'db' => 'CAGUA',    'publicacion' => 'CAGUA'],
+    'CARACAS 1'    => ['ip' => '26.163.49.171',         'db' => 'CARACAS1', 'publicacion' => 'CARACAS1'],
+    'CORO 1'       => ['ip' => '26.40.138.208',         'db' => 'CORO1',    'publicacion' => 'CORO1'],
+    'CORO 2'       => ['ip' => '26.10.12.209',          'db' => 'CORO2',    'publicacion' => 'CORO2'],
+    'CORO 3'       => ['ip' => '26.20.32.25\CORINA2',   'db' => 'CORO3',    'publicacion' => 'CORO3'],
+    'GUIGUE'       => ['ip' => '26.160.180.159\CATICA2','db' => 'GUIGE_A',  'publicacion' => 'GUIGUE'],
+    'HIGUEROTE'    => ['ip' => '26.185.48.247',         'db' => 'HIGUE',    'publicacion' => 'HIGUE'],
+    'MATURIN'      => ['ip' => '26.25.243.193',         'db' => 'MATURIN',  'publicacion' => 'MATURIN'],
+    'OJEDA'        => ['ip' => '26.173.204.113\OJENA',  'db' => 'OJEDA',    'publicacion' => 'OJEDA'],
+    'PUNTO FIJO 1' => ['ip' => '26.248.191.230\PUFIJO', 'db' => 'PTOFIJO1', 'publicacion' => 'PTOFIJO1'],
+    'PUNTO FIJO 2' => ['ip' => '26.229.9.68\NACHARI',   'db' => 'PTOFIJO2', 'publicacion' => 'PTOFIJO2'],
+    'PUERTO'       => ['ip' => '26.80.3.26',            'db' => 'PUERTOX',  'publicacion' => 'PUERTO'],
     'VALENCIA'     => ['ip' => '26.42.16.172\VALENA',   'db' => 'VALE_A21', 'publicacion' => 'VALENA'],
     'VALLE PASCUA' => ['ip' => '26.214.82.1\VALLE',     'db' => 'VAPASCUA', 'publicacion' => 'VAPASCUA']
+]; */
+
+
+$lista_replicas = [
+    // --- ESTAS 2 DEBEN FUNCIONAR (Datos Reales) ---
+    'ACARIGUA'     => ['ip' => '26.35.57.24',           'db' => 'ACARIGUA', 'publicacion' => 'ACARIGUA'],
+    'APURE'        => ['ip' => '26.77.57.145',          'db' => 'APURA',    'publicacion' => 'APURA'],
+
+    // --- ESTAS DARÁN ERROR (IPs Falsas para prueba) ---
+    'CAGUA'        => ['ip' => '0.0.0.0', 'db' => 'CAGUA',    'publicacion' => 'CAGUA'],
+    'CARACAS 1'    => ['ip' => '0.0.0.0', 'db' => 'CARACAS1', 'publicacion' => 'CARACAS1'],
+    'CORO 1'       => ['ip' => '0.0.0.0', 'db' => 'CORO1',    'publicacion' => 'CORO1'],
+    'CORO 2'       => ['ip' => '0.0.0.0', 'db' => 'CORO2',    'publicacion' => 'CORO2'],
+    'CORO 3'       => ['ip' => '0.0.0.0', 'db' => 'CORO3',    'publicacion' => 'CORO3'],
+    'GUIGUE'       => ['ip' => '0.0.0.0', 'db' => 'GUIGE_A',  'publicacion' => 'GUIGUE'],
+    'HIGUEROTE'    => ['ip' => '0.0.0.0', 'db' => 'HIGUE',    'publicacion' => 'HIGUE'],
+    'MATURIN'      => ['ip' => '0.0.0.0', 'db' => 'MATURIN',  'publicacion' => 'MATURIN'],
+    'OJEDA'        => ['ip' => '0.0.0.0', 'db' => 'OJEDA',    'publicacion' => 'OJEDA'],
+    'PUNTO FIJO 1' => ['ip' => '0.0.0.0', 'db' => 'PTOFIJO1', 'publicacion' => 'PTOFIJO1'],
+    'PUNTO FIJO 2' => ['ip' => '0.0.0.0', 'db' => 'PTOFIJO2', 'publicacion' => 'PTOFIJO2'],
+    'PUERTO'       => ['ip' => '0.0.0.0', 'db' => 'PUERTOX',  'publicacion' => 'PUERTO'],
+    'VALENCIA'     => ['ip' => '0.0.0.0', 'db' => 'VALE_A21', 'publicacion' => 'VALENA'],
+    'VALLE PASCUA' => ['ip' => '0.0.0.0', 'db' => 'VAPASCUA', 'publicacion' => 'VAPASCUA']
 ];
 ?>
